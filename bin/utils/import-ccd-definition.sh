46f05a487b848288604f8641d546aabb2f231863
Faran Kardame <faran.kardame@version1.com>
2022-01-27 16:04:15 +0000

    Fixing Build Pipeline issue

--- bin/utils/import-ccd-definition.sh
+++ bin/utils/import-ccd-definition.sh
@@ -20,6 +20,5 @@ echo "Additional parameters: ${additionalParameters}"
 
 mkdir -p $(dirname ${definition_output_file})
 
-
 ${dir}/process-definition.sh ${definition_input_dir} ${definition_output_file} "${additionalParameters}"
 ${dir}/ccd-import-definition.sh ${definition_output_file}
