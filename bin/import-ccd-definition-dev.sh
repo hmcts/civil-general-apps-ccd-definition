6b2d86e4fbea70e261a8e5c2a2a8ea9503ad1418
Faran Kardame <faran.kardame@version1.com>
2022-01-31 22:18:42 +0000

    Fixing Jenkins Build Failure

--- bin/import-ccd-definition-dev.sh
+++ bin/import-ccd-definition-dev.sh
@@ -4,5 +4,4 @@ definition_input_dir=$(realpath 'ccd-definition')
 definition_output_file="$(realpath ".")/build/ccd-development-config/ccd-civil-general-apps-dev.xlsx"
 params="$@"
 
-
 ./bin/utils/import-ccd-definition-dev.sh "${definition_input_dir}" "${definition_output_file}" "${params}"
