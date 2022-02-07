dff23b1478e6dbab20a0c43a52ff3217b00fc314
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2022-02-07 15:49:29 +0000

    Implemented Hearing Details test

--- e2e/tests/generalApplication_test.js
+++ e2e/tests/generalApplication_test.js
@@ -13,21 +13,34 @@ Scenario('Create case for @ga', async ({api}) => {
 });
 
 Scenario('Applicant solicitor creates Single general application @ga', async ({I}) => {
-  let app = appTypes.slice(0, 1);
   await I.login(config.applicantSolicitorUser);
   await I.navigateToCaseDetails(caseNumber);
   caseId = await I.grabCaseNumber();
-  await I.makeAnApplication(app, caseNumber, 'no');
-  await verifyGeneralApplication(I, caseId, app);
+  await I.selectApplicationType(appTypes.slice(0, 1), caseNumber);
+  await I.selectConsentCheck('no');
+  await I.isUrgentApplication('yes');
+  await I.selectNotice('yes');
+  await I.enterApplicationDetails();
+  await I.fillHearingDetails('no', 'no', 'no', 'no', 'disabledAccess');
+  await I.selectPbaNumber('no');
+  await I.verifyCheckAnswerForm(caseNumber, 'no');
+  await I.submitApplication();
+  await verifyGeneralApplication(I, caseId, appTypes.slice(0, 1));
 }).retry(2);
 
 Scenario('Applicant solicitor creates Multiple general applications @ga', async ({I}) => {
-  let apps = appTypes.slice(0, 3);
   await I.login(config.applicantSolicitorUser);
   await I.navigateToCaseDetails(caseNumber);
   caseId = await I.grabCaseNumber();
-  await I.makeAnApplication(apps, caseNumber, 'yes');
-  await verifyGeneralApplication(I, caseId, apps);
+  await I.selectApplicationType(appTypes.slice(0, 3), caseNumber);
+  await I.selectConsentCheck('yes');
+  await I.isUrgentApplication('no');
+  await I.enterApplicationDetails();
+  await I.fillHearingDetails('yes', 'yes', 'yes', 'no', 'signLanguageInterpreter')
+  await I.selectPbaNumber('yes');
+  await I.verifyCheckAnswerForm(caseNumber, 'yes');
+  await I.submitApplication();
+  await verifyGeneralApplication(I, caseId, appTypes.slice(0, 3));
 }).retry(2);
 
 const verifyGeneralApplication = async (I, caseId, appType) => {
