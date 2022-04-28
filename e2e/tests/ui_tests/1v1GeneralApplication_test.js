0e47a292bbf4e91993fb167fccdae14920db6d63
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2022-04-28 12:26:16 +0100

    Added new e2e smoke tests

--- e2e/tests/ui_tests/1v1GeneralApplication_test.js
+++ e2e/tests/ui_tests/1v1GeneralApplication_test.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 const config = require('../../config.js');
 const mpScenario = 'ONE_V_ONE';
-const respondentStatus = 'Awaiting Respondent Response';
 const judgeDecisionStatus = 'Application Submitted - Awaiting Judicial Decision';
 const childCaseNum = () => `${childCaseNumber.split('-').join('')}`;
 
@@ -34,39 +33,6 @@ Scenario('GA for 1v1 - Make an order journey', async ({I, api}) => {
   console.log('Judges made a decision on case: ' + childCaseNum());
 }).retry(0);
 
-Scenario('GA for 1v1- respond to application - Request more information journey', async ({I, api}) => {
-  parentCaseNumber = await api.createClaimWithRepresentedRespondent(
-    config.applicantSolicitorUser, mpScenario);
-  console.log('Case created for general application: ' + parentCaseNumber);
-  await I.login(config.applicantSolicitorUser);
-  await I.navigateToCaseDetails(parentCaseNumber);
-  caseId = await I.grabCaseNumber();
-  await I.createGeneralApplication(
-    getAppTypes().slice(0, 5),
-    parentCaseNumber,
-    'no', 'no', 'yes', 'yes', 'yes', 'yes', 'no',
-    'signLanguageInterpreter');
-  console.log('General Application created: ' + parentCaseNumber);
-  await I.closeAndReturnToCaseDetails(caseId);
-  await I.clickAndVerifyTab('Applications', getAppTypes().slice(0, 5), 1);
-  await I.see(respondentStatus);
-  childCaseNumber = await I.grabChildCaseNumber();
-  await I.navigateToCaseDetails(childCaseNum());
-  await I.verifyApplicantSummaryPage();
-  childCaseId = await I.grabCaseNumber();
-  await I.login(config.defendantSolicitorUser);
-  await I.respondToApplication(childCaseNum(), 'yes', 'yes', 'yes', 'yes', 'no',
-    'signLanguageInterpreter', getAppTypes().slice(0, 5));
-  console.log('Org1 solicitor Responded to application: ' + childCaseNum());
-  await I.respCloseAndReturnToCaseDetails(childCaseId);
-  await I.navigateToCaseDetails(parentCaseNumber);
-  await I.clickOnTab('Applications');
-  await I.see(judgeDecisionStatus);
-  await I.judgeRequestMoreInfo('requestMoreInfo', 'requestMoreInformation', childCaseNum());
-  await I.judgeCloseAndReturnToCaseDetails(childCaseId);
-  console.log('Judges requested more information on case: ' + childCaseNum());
-}).retry(0);
-
-AfterSuite(async  ({api}) => {
+AfterSuite(async ({api}) => {
   await api.cleanUp();
 });
--- e2e/tests/ui_tests/smoke_test.js
+++ e2e/tests/ui_tests/smoke_test.js
@@ -1,15 +1,48 @@
+/* eslint-disable no-unused-vars */
 const config = require('../../config.js');
 const mpScenario = 'ONE_V_ONE';
-let caseNumber, caseId;
+const respondentStatus = 'Awaiting Respondent Response';
+const judgeDecisionStatus = 'Application Submitted - Awaiting Judicial Decision';
+const childCaseNum = () => `${childCaseNumber.split('-').join('')}`;
+
 let {getAppTypes} = require('../../pages/generalApplication/generalApplicationTypes');
+let parentCaseNumber, caseId, childCaseId, childCaseNumber;
 
-Feature('General Application @smoke-tests');
+Feature('General Application end to end journey @smoke-tests');
 
-Scenario('Applicant solicitor should be able to go to General app screen', async ({I, api}) => {
-  caseNumber = await api.createClaimWithRepresentedRespondent(
+Scenario('GA for 1v1- respond to application - Request more information', async ({I, api}) => {
+  parentCaseNumber = await api.createClaimWithRepresentedRespondent(
     config.applicantSolicitorUser, mpScenario);
+  console.log('Case created for general application: ' + parentCaseNumber);
   await I.login(config.applicantSolicitorUser);
-  await I.navigateToCaseDetails(caseNumber);
+  await I.navigateToCaseDetails(parentCaseNumber);
   caseId = await I.grabCaseNumber();
-  await I.goToGeneralAppScreenAndVerifyAllApps(getAppTypes(), caseId);
-}).retry(1);
+  await I.createGeneralApplication(
+    getAppTypes().slice(0, 5),
+    parentCaseNumber,
+    'no', 'no', 'yes', 'yes', 'yes', 'yes', 'no',
+    'signLanguageInterpreter');
+  console.log('General Application created: ' + parentCaseNumber);
+  await I.closeAndReturnToCaseDetails(caseId);
+  await I.clickAndVerifyTab('Applications', getAppTypes().slice(0, 5), 1);
+  await I.see(respondentStatus);
+  childCaseNumber = await I.grabChildCaseNumber();
+  await I.navigateToCaseDetails(childCaseNum());
+  await I.verifyApplicantSummaryPage();
+  childCaseId = await I.grabCaseNumber();
+  await I.login(config.defendantSolicitorUser);
+  await I.respondToApplication(childCaseNum(), 'yes', 'yes', 'yes', 'yes', 'no',
+    'signLanguageInterpreter', getAppTypes().slice(0, 5));
+  console.log('Org1 solicitor Responded to application: ' + childCaseNum());
+  await I.respCloseAndReturnToCaseDetails(childCaseId);
+  await I.navigateToCaseDetails(parentCaseNumber);
+  await I.clickOnTab('Applications');
+  await I.see(judgeDecisionStatus);
+  await I.judgeRequestMoreInfo('requestMoreInfo', 'requestMoreInformation', childCaseNum());
+  await I.judgeCloseAndReturnToCaseDetails(childCaseId);
+  console.log('Judges requested more information on case: ' + childCaseNum());
+}).retry(0);
+
+AfterSuite(async ({api}) => {
+  await api.cleanUp();
+});
