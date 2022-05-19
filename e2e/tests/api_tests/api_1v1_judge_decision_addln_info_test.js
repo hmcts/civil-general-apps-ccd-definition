1e5143217c823761b02c8b1692282174916a1f7b
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2022-05-19 16:40:09 +0100

    refactory no. of scenarios

--- e2e/tests/api_tests/api_1v1_judge_decision_addln_info_test.js
+++ e2e/tests/api_tests/api_1v1_judge_decision_addln_info_test.js
@@ -4,23 +4,18 @@ const mpScenario = 'ONE_V_ONE';
 
 let civilCaseReference, gaCaseReference;
 
-Feature('GA 1v1 Judge Make Decision Addln Info Required API tests @api-tests');
+Feature('GA 1v1 Judge Make Decision Additional Information Required API tests @api-tests');
 
-Scenario('Initiate General application for 1v1', async ({api}) => {
-  civilCaseReference = await api.createClaimWithRepresentedRespondent(
-    config.applicantSolicitorUser, mpScenario);
+Scenario('Judge makes decision 1V1 - AWAITING_ADDITIONAL_INFORMATION', async ({api}) => {
+  civilCaseReference = await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
   console.log('Civil Case created for general application: ' + civilCaseReference);
   console.log('Make a General Application');
   gaCaseReference = await api.initiateGeneralApplication(config.applicantSolicitorUser, civilCaseReference);
-});
 
-Scenario('Respondent response for 1V1', async ({api}) => {
   console.log('*** Start response to GA Case Reference: ' + gaCaseReference + ' ***');
   await api.respondentResponse(config.defendantSolicitorUser, gaCaseReference);
   console.log('*** End Response to GA Case Reference: ' + gaCaseReference + ' ***');
-});
 
-Scenario('Judge makes decision 1V1', async ({api}) => {
   console.log('*** Start Judge Make Decision on GA Case Reference: ' + gaCaseReference + ' ***');
   await api.judgeMakesDecision(config.applicantSolicitorUser, gaCaseReference);
   console.log('*** End Judge Make Decision GA Case Reference: ' + gaCaseReference + ' ***');
--- e2e/tests/api_tests/api_1v1_written_rep_test.js
+++ e2e/tests/api_tests/api_1v1_written_rep_test.js
@@ -6,21 +6,17 @@ let civilCaseReference, gaCaseReference;
 
 Feature('GA 1v1 Judge Make Order Written Rep API tests @api-tests');
 
-Scenario('Initiate General application for 1v1', async ({api}) => {
+Scenario('Judge makes decision 1V1 - WRITTEN_REPRESENTATIONS', async ({api}) => {
   civilCaseReference = await api.createClaimWithRepresentedRespondent(
     config.applicantSolicitorUser, mpScenario);
   console.log('Civil Case created for general application: ' + civilCaseReference);
   console.log('Make a General Application');
   gaCaseReference = await api.initiateGeneralApplication(config.applicantSolicitorUser, civilCaseReference);
-});
 
-Scenario('Respondent response for 1V1', async ({api}) => {
   console.log('*** Start response to GA Case Reference: ' + gaCaseReference + ' ***');
   await api.respondentResponse(config.defendantSolicitorUser, gaCaseReference);
   console.log('*** End Response to GA Case Reference: ' + gaCaseReference + ' ***');
-});
 
-Scenario('Judge makes decision 1V1 - WRITTEN_REPRESENTATIONS', async ({api}) => {
   console.log('*** Start Judge Make Order on GA Case Reference - WRITTEN_REPRESENTATIONS: ' + gaCaseReference + ' ***');
   await api.judgeMakesDecisionWrittenRep(config.applicantSolicitorUser, gaCaseReference);
   console.log('*** End Judge Make Order GA Case Reference - WRITTEN_REPRESENTATIONS: ' + gaCaseReference + ' ***');
