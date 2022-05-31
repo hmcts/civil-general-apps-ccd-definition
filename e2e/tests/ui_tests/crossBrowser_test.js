4932f2171375e41d61bf43864f60d2ef6d55ed1a
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2022-05-31 13:39:10 +0100

    Initial commit

--- e2e/tests/ui_tests/crossBrowser_test.js
+++ e2e/tests/ui_tests/crossBrowser_test.js
@@ -1,39 +1,58 @@
+/* eslint-disable no-unused-vars */
 const config = require('../../config.js');
 const mpScenario = 'ONE_V_ONE';
-const appStatus = 'Awaiting Respondent Response';
+const respondentStatus = 'Awaiting Respondent Response';
+const judgeDecisionStatus = 'Application Submitted - Awaiting Judicial Decision';
+const additionalInfoStatus = 'Additional Information Required';
 const childCaseNum = () => `${childCaseNumber.split('-').join('')}`;
+const {waitForGACamundaEventsFinishedBusinessProcess} = require('../../api/testingSupport');
 
 let {getAppTypes} = require('../../pages/generalApplication/generalApplicationTypes');
-let caseNum, caseId, childCaseId, childCaseNumber;
-
+let parentCaseNumber, caseId, childCaseId, childCaseNumber, gaCaseReference;
 Feature('End-to-end General application journey @cross-browser-tests');
 
-Scenario('GA end-to-end journey', async ({I, api}) => {
-  // To do
-  caseNum = await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, mpScenario);
-  console.log('Case created for general application: ' + caseNum);
+Scenario('End to End Judges Journey', async ({I, api}) => {
+  parentCaseNumber = await api.createClaimWithRepresentedRespondent(
+    config.applicantSolicitorUser, mpScenario);
+  console.log('Case created for general application: ' + parentCaseNumber);
   await I.login(config.applicantSolicitorUser);
-  await I.navigateToCaseDetails(caseNum);
+  await I.navigateToCaseDetails(parentCaseNumber);
   caseId = await I.grabCaseNumber();
   await I.createGeneralApplication(
     getAppTypes().slice(0, 5),
-    caseNum,
+    parentCaseNumber,
     'no', 'no', 'yes', 'yes', 'yes', 'yes', 'no',
     'signLanguageInterpreter');
-  console.log('General Application created: ' + caseNum);
+  console.log('General Application created: ' + parentCaseNumber);
+  gaCaseReference = await api.getGACaseReference(config.applicantSolicitorUser, parentCaseNumber);
+  await waitForGACamundaEventsFinishedBusinessProcess(gaCaseReference, 'AWAITING_RESPONDENT_RESPONSE');
   await I.closeAndReturnToCaseDetails(caseId);
-  await I.clickAndVerifyTab('Applications', getAppTypes().slice(0, 5), 1);
-  await I.see(appStatus);
-  // Refactor as part of CIV-1425
+  await I.clickAndVerifyTab(parentCaseNumber, 'Applications', getAppTypes().slice(0, 5), 1);
+  await I.see(respondentStatus);
   childCaseNumber = await I.grabChildCaseNumber();
+  await I.navigateToCaseDetails(childCaseNum());
+  await I.verifyApplicantSummaryPage();
+  childCaseId = await I.grabCaseNumber();
+  await I.login(config.defendantSolicitorUser);
   await I.respondToApplication(childCaseNum(), 'yes', 'yes', 'yes', 'yes', 'no',
     'signLanguageInterpreter', getAppTypes().slice(0, 5));
-  console.log('Responded to application: ' + childCaseNum());
-  childCaseId = await I.grabGACaseNumber();
+  console.log('Org1 solicitor Responded to application: ' + childCaseNum());
   await I.respCloseAndReturnToCaseDetails(childCaseId);
-  await I.verifyResponseSummaryPage();
-  await I.judgeRequestMoreInfo('requestMoreInfo', 'sendApplicationToOtherParty', childCaseNum());
+  await I.navigateToTab(parentCaseNumber, 'Applications');
+  await I.see(judgeDecisionStatus);
+  await I.judgeRequestMoreInfo('requestMoreInfo', 'requestMoreInformation', childCaseNum());
+  await waitForGACamundaEventsFinishedBusinessProcess(gaCaseReference, 'JUDGE_MAKES_DECISION');
   await I.judgeCloseAndReturnToCaseDetails(childCaseId);
-  console.log('Judges sent application to other party and requested hearing details on case: ' + childCaseNum());
-}).retry(0);
+  await I.verifyJudgesSummaryPage('Request more information');
+  await I.verifyApplicationDocument(childCaseNum(), 'Request for information');
+  console.log('Judges requested more information on case: ' + childCaseNum());
+  await I.navigateToTab(parentCaseNumber, 'Applications');
+  await I.see(additionalInfoStatus);
+  await I.respondToJudgeAdditionalInfo(childCaseNum(), childCaseId);
+  console.log('Responded to Judge Additional Information on case: ' + childCaseNum());
+}).retry(1);
+
+AfterSuite(async ({api}) => {
+  await api.cleanUp();
+});
 
