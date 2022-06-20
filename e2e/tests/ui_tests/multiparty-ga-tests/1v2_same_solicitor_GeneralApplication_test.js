a3b648081796f595bc9b5b9fb6c64ed2c26edf7b
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2022-06-20 17:26:03 +0100

    added test for CIV-2407

--- e2e/tests/ui_tests/multiparty-ga-tests/1v2_same_solicitor_GeneralApplication_test.js
+++ e2e/tests/ui_tests/multiparty-ga-tests/1v2_same_solicitor_GeneralApplication_test.js
@@ -66,25 +66,18 @@ Scenario('GA for 1v2 Same Solicitor - Send application to other party journey',
     await I.createGeneralApplication(
       getAppTypes().slice(0, 5),
       parentCaseNum,
-      'no', 'no', 'yes', 'yes', 'yes', 'yes', 'no',
+      'no', 'no', 'no', 'yes', 'yes', 'yes', 'no',
       'signLanguageInterpreter');
     console.log('General Application created: ' + parentCaseNum);
     gaCaseReference = await api.getGACaseReference(config.applicantSolicitorUser, parentCaseNum);
     await waitForGACamundaEventsFinishedBusinessProcess(gaCaseReference, 'AWAITING_RESPONDENT_RESPONSE');
     await I.closeAndReturnToCaseDetails(caseId);
     await I.clickAndVerifyTab(parentCaseNum, 'Applications', getAppTypes().slice(0, 5), 1);
-    await I.see(respondentStatus);
-    childCaseNumber = await I.grabChildCaseNumber();
-    await I.login(config.defendantSolicitorUser);
-    await I.respondToApplication(childCaseNum(), 'yes', 'yes', 'yes', 'yes', 'no',
-      'signLanguageInterpreter', getAppTypes().slice(0, 5));
-    console.log('Org1 solicitor responded to application: ' + childCaseNum());
-    childCaseId = await I.grabGACaseNumber();
-    await I.respCloseAndReturnToCaseDetails(childCaseId);
-    await I.verifyResponseSummaryPage();
-    await I.navigateToTab(parentCaseNum, 'Applications');
     await I.see(judgeDecisionStatus);
-    await I.judgeRequestMoreInfo('requestMoreInfo', 'sendApplicationToOtherParty', childCaseNum());
+    childCaseNumber = await I.grabChildCaseNumber();
+    await I.navigateToCaseDetails(childCaseNum());
+    childCaseId = await I.grabCaseNumber();
+    await I.judgeRequestMoreInfo('requestMoreInfo', 'sendApplicationToOtherParty', childCaseNum(), 'no');
     await waitForGACamundaEventsFinishedBusinessProcess(gaCaseReference, 'JUDGE_MAKES_DECISION');
     await I.judgeCloseAndReturnToCaseDetails(childCaseId);
     console.log('Judges sent application to other party and requested hearing details on case: ' + childCaseNum());
