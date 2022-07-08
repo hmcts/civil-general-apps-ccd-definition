9e6c1ec5caa7b299be77098487ba1451b8ca6714
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-07-08 21:56:36 +0100

    Update responseConfirmation.page.js

--- e2e/pages/generalApplication/responseJourneyPages/responseConfirmation.page.js
+++ e2e/pages/generalApplication/responseJourneyPages/responseConfirmation.page.js
@@ -27,6 +27,7 @@ module.exports = {
   async closeAndReturnToCaseDetails(childCaseId) {
     await I.click('Close and Return to case details');
     await I.waitForInvisible(locate('.loading-spinner-in-action').withText('Loading'), 5);
+    console.log('Child case id ' + childCaseId);
     //await I.see(`Case ${childCaseId} has been updated with event: Respond to application`);
   },
 
