b044a9d663a2cca609f3037b6ec824b13e97c5b5
dawidWalter <dawid.walter@hmcts.net>
2022-07-14 13:50:49 +0100

    1v2  representation scenario, change to one legal rep

--- e2e/tests/api_tests/api_1v2_awaiting_written_representation.js
+++ e2e/tests/api_tests/api_1v2_awaiting_written_representation.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 const config = require('../../config.js');
-const mpScenario = 'ONE_V_TWO_TWO_LEGAL_REP';
+const mpScenario = 'ONE_V_TWO_ONE_LEGAL_REP';
 
 let civilCaseReference, gaCaseReference;
 
