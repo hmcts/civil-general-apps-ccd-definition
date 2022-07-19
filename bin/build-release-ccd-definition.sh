b748a0d6d65b783e90717ce415ea210ef8471112
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-07-19 11:09:27 +0100

    prevent prod run

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -21,10 +21,12 @@ github_dir=${root_dir}/build/github-release
 release_definition_output_file=${build_dir}/civil-ccd-${environment}.xlsx
 github_file=${github_dir}/civil-ccd-${environment}.xlsx
 
-mkdir -p ${build_dir}
-mkdir -p ${github_dir}
+if [ ${environment} != prod ]; then
+  mkdir -p ${build_dir}
+  mkdir -p ${github_dir}
 
-# build the ccd definition file
-${root_dir}/bin/utils/process-definition.sh ${config_dir} ${release_definition_output_file} "${excludedFilenamePatterns}"
+  # build the ccd definition file
+  ${root_dir}/bin/utils/process-definition.sh ${config_dir} ${release_definition_output_file} "${excludedFilenamePatterns}"
 
-cp ${release_definition_output_file} ${github_file}
+  cp ${release_definition_output_file} ${github_file}
+fi
