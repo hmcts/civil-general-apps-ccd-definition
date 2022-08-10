c9dc5d23d667d66816f09822a7de18d0f2fedcc3
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2022-08-10 12:22:19 +0100

    reverted back to local urls

--- e2e/config.js
+++ e2e/config.js
@@ -6,17 +6,17 @@ module.exports = {
     url: 'http://localhost:5555'
   },
   url: {
-    manageCase: process.env.URL || 'https://manage-case.demo.platform.hmcts.net',
-    authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
-    ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://ccd-data-store-api-demo.service.core-compute-demo.internal',
-    dmStore: process.env.DM_STORE_URL || 'http://dm-store-demo.service.core-compute-demo.internal',
-    idamApi: process.env.IDAM_API_URL || 'https://idam-api.demo.platform.hmcts.net',
-    civilService: process.env.CIVIL_SERVICE_URL || 'http://civil-service-demo.service.core-compute-demo.internal',
-    generalApplication: process.env.CIVIL_GENERAL_APPLICATIONS_URL  || 'http://civil-general-applications-demo.service.core-compute-demo.internal',
+    manageCase: process.env.URL || 'http://localhost:3333',
+    authProviderApi: process.env.SERVICE_AUTH_PROVIDER_API_BASE_URL || 'http://localhost:4502',
+    ccdDataStore: process.env.CCD_DATA_STORE_URL || 'http://localhost:4452',
+    dmStore: process.env.DM_STORE_URL || 'http://dm-store:8080',
+    idamApi: process.env.IDAM_API_URL || 'http://localhost:5000',
+    civilService: process.env.CIVIL_SERVICE_URL || 'http://localhost:4000',
+    generalApplication: process.env.CIVIL_GENERAL_APPLICATIONS_URL  || 'http://localhost:4550',
   },
   s2s: {
     microservice: 'civil_service',
-    secret: process.env.S2S_SECRET || 'AABBCCDDEEFFGGHH'
+    secret: process.env.S2S_SECRET || 'RD53IBEKQTO4WMQY'
   },
   applicantSolicitorUser: {
     password: defaultPassword,
