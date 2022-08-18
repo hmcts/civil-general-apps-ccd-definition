9002e3ea643be48686a92f07d0a625102a4b77dc
Azam <106387766+Azam-Hmcts@users.noreply.github.com>
2022-08-18 14:07:20 +0100

    Civ 3760 uncloaking without notice make order (#282)
    * CIV-3608:Added new state for additional paymment
    
    * CIV-3068: Added camunda process for addition payment reference
    
    * CIV-3068:Added new field for additional payment service reference
    
    * CIV-3408: Added new camunda event for addition fee value and done refactorization
    
    * CIV-3405
    
    Added new callback handler and changed the name of existing handler
    
    * CIV-3408: Added new fields and authorizations for callback handlers
    
    * CIV-3405
    
    Added a new field Service request reference in General PBA Details
    
    * CIV-3405
    
    ServiceRequestReferernce element incorrectly tagged in gaccd from general app
    
    * CIV-3405
    
    Reverting the last commit as it was by mistake to change the property tag
    
    * CIV-3408: Added api test for Application add payment
    
    - Added configuration for new camunda task OBTAIN_ADDITIONAL_PAYMENT_REF
    
    * CIV-3408: Added call back url for additional payment calls
    
    * Bumping chart version/ fixing aliases
    
    * CIV-3663 Add Recital text judge request more info
    
    * Add recital field - smoke test
    
    * CIV-3408: Updated description of Callback
    
    * CIV-3408: Removed duplicate code
    
    * Front end automation changes for 3408
    
    * removed unused methods
    
    * CIV-3408: Updated and Added new api test
    
    * CIV-3408: Updated method name in functional testing
    
    * CIV-3408: Modified  configuration for passing the branch 218
    
    * CIV-3408: Removed unused code
    
    * CIV-3408: Correct image tag for preview environment
    
    * CIV-3408: updated image version for general applications
    
    * CIV-3408 : Removed test deletion after creation
    
    * CIV-3408: Updated url for PAY_CALLBACK_URL
    
    * CIV-3408: Modified https call to http for PAY_CALLBACK_URL
    
    * CIV-3663 Fix Functional test
    
    * added test for Additional payment
    
    * CIV-3663 automation changes
    
    * CIV-3408: Removed Application_Add_Payment state
    
    * Bumping chart version/ fixing aliases
    
    * CIV-3760: Remove additonal payment status from functional test
    
    * CIV3760: Removed test branch configuration
    
    * CIV3760: Corrected camunda branch
    
    * CIV-3760: Removed indentation changes
    
    * Automation changes for 3760
    
    * CIV-3760: Modified Container Image tag for general application
    
    * CIV-3760: Fixed error test error after merge of  3854
    
    * CIV-3760: Resolved commented code issue
    
    * CIV-3760: After Passing PR, GA image set to latest
    
    * CIV-3760: Removed local test configuration
    
    Co-authored-by: Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
    Co-authored-by: hmcts-jenkins-a-to-c <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
    Co-authored-by: karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
    Co-authored-by: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Co-authored-by: Madhan Mahadevan <madhan.mahadevan@hmcts.net>
    Co-authored-by: Jenkins <nobody@nowhere>

--- e2e/tests/api_unspec_claim_tests/api_1v1_judge_decision_uncloak_application_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_judge_decision_uncloak_application_test.js
@@ -6,7 +6,7 @@ let civilCaseReference, gaCaseReference;
 
 Feature('GA 1v1 Judge Make Decision Uncloak application API  @api-tests');
 
-Scenario('Judge makes decision 1V1 - APPLICATION_ADD_PAYMENT ', async ({api}) => {
+Scenario('Judge makes decision 1V1 - ORDER_MADE - Uncloak and Application Approved', async ({api}) => {
 
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
@@ -23,34 +23,8 @@ Scenario('Judge makes decision 1V1 - APPLICATION_ADD_PAYMENT ', async ({api}) =>
   console.log('*** End Judge Make Decision Uncloak and Application Approved on GA Case Reference: '
     + gaCaseReference + ' ***');
 
-  console.log('*** Start Callback for Additional Payment: ' + gaCaseReference + ' ***');
-  await api.additionalPaymentSuccess(config.applicantSolicitorUser, gaCaseReference);
-  console.log('*** End Judge Make Decision Application Dismiss on GA Case Reference: ' + gaCaseReference + ' ***');
-
 });
 
-Scenario('Judge makes decision 1V1 - APPLICATION_ADD_PAYMENT - Payment fails', async ({api}) => {
-
-  civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario);
-  await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
-  await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
-
-  console.log('Civil Case created for general application: ' + civilCaseReference);
-  console.log('Make a General Application without notice');
-  gaCaseReference = await api.initiateGeneralApplicationWithOutNotice(config.applicantSolicitorUser,
-    civilCaseReference);
-
-  console.log('*** Start Judge Make Decision Uncloak and Application Approved on GA Case Reference: '
-    + gaCaseReference + ' ***');
-  await api.judgeMakesDecisionApplicationUncloak(config.applicantSolicitorUser, gaCaseReference);
-  console.log('*** End Judge Make Decision Uncloak and Application Approved on GA Case Reference: '
-    + gaCaseReference + ' ***');
-
-  console.log('*** Start Callback for Additional Payment Failure: ' + gaCaseReference + ' ***');
-  await api.additionalPaymentFailure(config.applicantSolicitorUser, gaCaseReference);
-  console.log('*** End Callback for Additional Payment Failure on GA Case Reference: ' + gaCaseReference + ' ***');
-
-});
 AfterSuite(async ({api}) => {
   await api.cleanUp();
 });
