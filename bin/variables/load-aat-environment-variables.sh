b8d2149222e4bffd953280a4c686dcddfb2ea5c1
smita.talawadekar <smita.talawadekar@HMCTS.NET>
2022-09-06 16:46:38 +0100

    CIV-4462 service names

--- bin/variables/load-aat-environment-variables.sh
+++ bin/variables/load-aat-environment-variables.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-general-apps-ccd-definition-staging-civil-service"
-echo "export CCD_DEF_GEN_APP_SERVICE_BASE_URL=http://civil-general-apps-ccd-definition-staging-java"
+echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-ga-ccd-definition-staging-civil-service"
+echo "export CCD_DEF_GEN_APP_SERVICE_BASE_URL=http://civil-ga-ccd-definition-staging-java"
