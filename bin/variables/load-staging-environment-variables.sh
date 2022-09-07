f25416341df12abba6761128a5595d1b3d578777
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2022-09-07 13:37:24 +0100

    service_name changes

--- bin/variables/load-staging-environment-variables.sh
+++ bin/variables/load-staging-environment-variables.sh
@@ -12,5 +12,5 @@ echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://civil-ga-ccd-definition-s
 echo "export CAMUNDA_BASE_URL=https://civil-ga-ccd-camunda-staging-aat.service.core-compute-aat.internal"
 
 # definition placeholders
-echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-general-apps-ccd-definition-staging-civil-service"
-echo "export CCD_DEF_GEN_APP_SERVICE_BASE_URL=http://civil-general-apps-ccd-definition-staging-java"
+echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-ga-ccd-staging-civil-service"
+echo "export CCD_DEF_GEN_APP_SERVICE_BASE_URL=http://civil-ga-ccd-staging-java"
