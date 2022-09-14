5a57d35aa47b3b7b1f2ce0f27d3d0b05d94c298d
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-09-14 11:48:51 +0100

    Update add-roles.sh

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# User used during the CCD import and ccd-role creations
+# User used during the CCD import and ccd-role creation
 ./bin/utils/ccd-add-role.sh "caseworker-civil"
 ./bin/utils/ccd-add-role.sh "caseworker-caa"
 ./bin/utils/ccd-add-role.sh "caseworker-approver"
