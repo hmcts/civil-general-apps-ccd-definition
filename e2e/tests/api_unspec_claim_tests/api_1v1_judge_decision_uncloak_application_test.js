dcf0a28456384d07e0cdab4a5e694873fbbe2a0d
Pijian Liao <Pijian.Liao@HMCTS.NET>
2022-10-04 17:55:33 +0100

    CIV-5085 add api test case for update ga status with visible to resp sol

--- e2e/tests/api_unspec_claim_tests/api_1v1_judge_decision_uncloak_application_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_judge_decision_uncloak_application_test.js
@@ -71,6 +71,39 @@ Scenario('Judge makes decision 1V1 - REQUEST_MORE_INFORMATION - Payment fails', 
 
 });
 
+Scenario('Judge makes decision 1V1 - REQUEST_MORE_INFORMATION - Uncloak Application - Response - Directions Order', async ({api}) => {
+
+  civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario);
+  await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
+  await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
+
+  console.log('Civil Case created for general application: ' + civilCaseReference);
+  console.log('Make a General Application without notice');
+  gaCaseReference = await api.initiateGeneralApplicationWithOutNotice(config.applicantSolicitorUser,
+                                                                      civilCaseReference);
+
+  console.log('*** Start Judge Request More Information and Uncloak Application on GA Case Reference: '
+              + gaCaseReference + ' ***');
+  await api.judgeRequestMoreInformationUncloak(config.applicantSolicitorUser, gaCaseReference);
+  console.log('*** End Judge Request More Information and Uncloak Application on GA Case Reference: '
+              + gaCaseReference + ' ***');
+
+  console.log('*** Start Callback for Additional Payment: ' + gaCaseReference + ' ***');
+  await api.additionalPaymentSuccess(config.applicantSolicitorUser, gaCaseReference, 'AWAITING_RESPONDENT_RESPONSE');
+  console.log('*** End Callback for Additional Payment on GA Case Reference: ' + gaCaseReference + ' ***');
+
+  console.log('*** Start Respondent respond to Judge Additional information on GA Case Reference: '
+              + gaCaseReference + ' ***');
+  await api.respondentResponse(config.defendantSolicitorUser, gaCaseReference);
+  console.log('*** End Respondent respond to Judge Additional information on GA Case Reference: '
+              + gaCaseReference + ' ***');
+
+  console.log('*** Start Judge Make Decision on GA Case Reference: ' + gaCaseReference + ' ***');
+  await api.judgeMakesDecisionDirectionsOrder(config.applicantSolicitorUser, gaCaseReference);
+  console.log('*** End Judge Make Decision GA Case Reference: ' + gaCaseReference + ' ***');
+
+});
+
 AfterSuite(async ({api}) => {
   await api.cleanUp();
 });
