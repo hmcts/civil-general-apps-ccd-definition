cc7e0b25ac840217d1e388faa513fa649e84aa14
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2022-10-05 14:34:04 +0100

    CIV-2801 testing branch

--- bin/import-ccd-definition.sh
+++ bin/import-ccd-definition.sh
@@ -5,7 +5,20 @@ set -ex
 
 definition_input_dir=$(realpath 'ccd-definition')
 definition_output_file="$(realpath ".")/build/ccd-development-config/ccd-civil-dev.xlsx"
-params="$@"
+
+params="$1"
+branchName="$2"
+
+#Checkout specific branch of civil ccd definition
+git clone https://github.com/hmcts/civil-ccd-definition.git -b ${branchName}
+cd civil-ccd-definition
+
+#echo "Switch to ${branchName} branch on civil-ccd-definition"
+#git checkout ${branchName}
+
+
+definition_input_dir=$(realpath './ccd-definition')
+definition_output_file="$(realpath ".")/ccd-definition/build/ccd-development-config/ccd-civil-dev.xlsx"
 
 params="$1"
 branchName="$2"
