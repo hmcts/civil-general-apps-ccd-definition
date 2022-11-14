c47c18ae445846aecbaf6336d4c69eb706cc1989
Pijian Liao <Pijian.Liao@HMCTS.NET>
2022-11-14 17:45:56 +0000

    CIV-4936 fix Case offline 1V2 AWAITING_RESPONDENT_RESPONSE offline

--- e2e/tests/api_unspec_claim_tests/api_1v1_claimant_response_offline_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_claimant_response_offline_test.js
@@ -21,7 +21,7 @@ Scenario('Case offline 1V2 AWAITING_RESPONDENT_RESPONSE', async ({api}) => {
     console.log('*** Case offline: ' + civilCaseReference + ' ***');
     await api.defendantResponseClaim(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
     await api.claimantResponseClaim(config.applicantSolicitorUser, 'NOT_PROCEED', 'ONE_V_TWO',
-                                    'JUDICIAL_REFERRAL');
+                                    'AWAITING_APPLICANT_INTENTION');
     await api.verifyGAState(config.applicantSolicitorUser, civilCaseReference, gaCaseReference, 'PROCEEDS_IN_HERITAGE');
 });
 
