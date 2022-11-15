7f200e174f3234c32cb390cea7bf9d5ede9faf8e
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-11-15 12:26:07 +0000

    Update HighLevelDataSetupApp.java
    enable tolerate

--- src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
@@ -81,4 +81,9 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
         // Do not create role assignments.
         BeftaUtils.defaultLog("Will NOT create role assignments!");
     }
+
+    @Override
+    protected boolean shouldTolerateDataSetupFailure() {
+        return true;
+    }
 }
