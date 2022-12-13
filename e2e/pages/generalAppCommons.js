0cb4635099fef2b9a0ab4c5f12cf879f2945a7cc
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2022-12-13 09:44:02 +0000

    judges recital refactor

--- e2e/pages/generalAppCommons.js
+++ e2e/pages/generalAppCommons.js
@@ -6,20 +6,14 @@ const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', '
 const date = new Date();
 const twoDigitDate = ((date.getDate()) >= 10) ? (date.getDate()) : '0' + (date.getDate());
 let fullDate = twoDigitDate + ' ' + month[date.getMonth()] + ' ' + date.getFullYear().toString().substr(-2);
-let expectedRecitalForWithoutNotice = `Upon the application of Claimant dated ${fullDate} and upon considering the information provided by the Claimant`;
-let expectedRecitalForWithNotice = `Upon the application of Claimant dated ${fullDate} and upon considering the information provided by the parties`;
 
 module.exports = {
   verifyJudgeRecitalText: async (actualJudgeRecitalText, consentCheck) => {
-    if (['preview', 'demo', 'aat'].includes(config.runningEnv)) {
-      await expect(actualJudgeRecitalText).to.contains(apiRequest.getUserFullName(config.judgeUser));
+    let fullJudgeName = await apiRequest.getUserFullName(config.judgeUser);
+    if (consentCheck === 'yes') {
+      await expect(actualJudgeRecitalText).to.equals(`${fullJudgeName} \nUpon the application of Claimant dated ${fullDate} and upon considering the information provided by the Claimant`);
     } else {
-      await expect(actualJudgeRecitalText).to.contains(apiRequest.getUserFullName(config.judgeLocalUser));
-    }
-    if (consentCheck === 'no') {
-      await expect(actualJudgeRecitalText).to.contains(expectedRecitalForWithoutNotice);
-    } else {
-      await expect(actualJudgeRecitalText).to.contains(expectedRecitalForWithNotice);
+      await expect(actualJudgeRecitalText).to.equals(`${fullJudgeName} \nUpon the application of Claimant dated ${fullDate} and upon considering the information provided by the parties`);
     }
   }
 };
--- e2e/pages/generalApplication/judgesJourneyPages/judgesSummary.page.js
+++ e2e/pages/generalApplication/judgesJourneyPages/judgesSummary.page.js
@@ -1,5 +1,5 @@
+const {verifyJudgeRecitalText} = require('../../generalAppCommons');
 const {I} = inject();
-const apiRequest = require('../../../api/apiRequest.js');
 
 module.exports = {
 
@@ -7,30 +7,31 @@ module.exports = {
     summaryLabels: 'ccd-field-read-label ng-component span'
   },
 
-  async verifyJudgesSummaryPage(decisionType, user) {
-    let fullName = await apiRequest.getUserFullName(user);
+  async verifyJudgesSummaryPage(decisionType) {
     I.waitInUrl('#Summary');
     I.see('Summary');
     I.see('Parent Case ID');
     I.see('Hearing details');
     I.see('Preferred location');
     I.see('Vulnerability questions');
-    await I.see(fullName);
     switch (decisionType) {
       case 'Judges Directions':
         await I.see('Judge’s recital');
+        await verifyJudgeRecitalText(await I.grabTextFrom(locate(this.fields.summaryLabels).first()));
         await I.see('Reasons for decision');
         await I.see('Directions');
         await I.see('When should this application be referred to a Judge again?');
         break;
       case 'Concurrent representations':
         await I.see('Judge’s recital');
+        await verifyJudgeRecitalText(await I.grabTextFrom(locate(this.fields.summaryLabels).first()));
         await I.see('Make an order for written representations');
         await I.see('Concurrent representations');
         await I.see('Directions in relation to hearing');
         break;
       case 'Sequential representations':
         await I.see('Judge’s recital');
+        await verifyJudgeRecitalText(await I.grabTextFrom(locate(this.fields.summaryLabels).first()));
         await I.see('Make an order for written representations');
         await I.see('Sequential representations');
         await I.see('Directions in relation to hearing');
@@ -39,6 +40,7 @@ module.exports = {
         break;
       case 'Request more information':
         await I.see('Judge’s recital');
+        await verifyJudgeRecitalText(await I.grabTextFrom(locate(this.fields.summaryLabels).first()));
         await I.see('Request more Info - Judge recital text');
         await I.see('Enter the information required');
         await I.see('Request for information');
@@ -46,22 +48,26 @@ module.exports = {
         break;
       case 'Dismissal order':
         await I.see('Judge’s recital');
+        await verifyJudgeRecitalText(await I.grabTextFrom(locate(this.fields.summaryLabels).first()));
         await I.see('Judges dismissed the order');
         await I.see('Dismissal order');
         await I.see('Reasons for decision');
         break;
       case 'Approve order':
         await I.see('Judge’s recital');
+        await verifyJudgeRecitalText(await I.grabTextFrom(locate(this.fields.summaryLabels).first()));
         await I.see('Date for Order to end');
         await I.see('For which document?');
         await I.see('Reasons for decision');
         break;
       case 'Hearing order':
         await I.see('Judge’s recital');
+        await verifyJudgeRecitalText(await I.grabTextFrom(locate(this.fields.summaryLabels).first()));
         await I.see('Directions in relation to hearing');
         break;
       case 'Send application to other party':
         await I.see('Judge’s recital');
+        await verifyJudgeRecitalText(await I.grabTextFrom(locate(this.fields.summaryLabels).first()));
         await I.see('Send Application - Judge recital text');
         await I.see('Request for information');
         await I.see('Send application to other party and request hearing details');
