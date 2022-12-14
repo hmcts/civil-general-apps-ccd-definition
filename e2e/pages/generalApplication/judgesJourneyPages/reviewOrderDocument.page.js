7b470b9290e44eaa5aee96e7264aa0edfc3e29ba
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2022-12-14 15:51:25 +0000

    fix for functional test failure

--- e2e/pages/generalApplication/judgesJourneyPages/reviewOrderDocument.page.js
+++ e2e/pages/generalApplication/judgesJourneyPages/reviewOrderDocument.page.js
@@ -12,10 +12,10 @@ module.exports = {
   },
 
   async reviewOrderDocument(documentType, childCaseNumber) {
-    await I.waitForElement(this.fields.caseForm);
+    await I.waitForElement(this.fields.previewDocFields.caseForm);
     await I.seeInCurrentUrl('MAKE_DECISION/MAKE_DECISIONGAJudicial');
     I.seeNumberOfVisibleElements('.button', 2);
-    let docURL = await I.grabTextFrom(locate(this.fields.documentLink));
+    let docURL = await I.grabTextFrom(locate(this.fields.previewDocFields.documentLink));
     expect(docURL).to.equals(`${documentType}_for_application_${childCaseNumber}.pdf`);
     await I.seeTextEquals('Draft Order', this.fields.previewDocFields.caseFieldLabel);
     await I.clickContinue();
