641c629d4e03878012f8430b9e50f9f29863a752
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-01-18 12:25:10 +0000

    fix for CIV-6702

--- e2e/pages/generalApplication/judgesJourneyPages/reviewOrderDocument.page.js
+++ e2e/pages/generalApplication/judgesJourneyPages/reviewOrderDocument.page.js
@@ -13,15 +13,15 @@ module.exports = {
   },
 
   async reviewOrderDocument(documentType) {
-    await I.waitInUrl('MAKE_DECISION/MAKE_DECISIONGAJudicial');
     if (documentType !== 'sendApplicationToOtherParty') {
+      await I.waitInUrl('MAKE_DECISION/MAKE_DECISIONGAJudicial');
       await I.waitForElement(this.fields.previewDocFields.caseForm);
       I.seeNumberOfVisibleElements('.button', 2);
       let docURL = await I.grabTextFrom(locate(this.fields.previewDocFields.documentLink));
       expect(docURL).to.contains(`${documentType}_for_application_${docFullDate}`);
+      await I.seeTextEquals('Draft Order', this.fields.previewDocFields.caseFieldLabel);
+      await I.clickContinue();
     }
-    await I.seeTextEquals('Draft Order', this.fields.previewDocFields.caseFieldLabel);
-    await I.clickContinue();
   }
 };
 
