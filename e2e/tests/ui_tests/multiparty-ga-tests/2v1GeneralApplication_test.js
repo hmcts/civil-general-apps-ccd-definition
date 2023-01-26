7ee3fba2c77156ef5377200f3ac53763195be90a
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-01-26 16:12:48 +0000

    code cleanup

--- e2e/tests/ui_tests/multiparty-ga-tests/2v1GeneralApplication_test.js
+++ e2e/tests/ui_tests/multiparty-ga-tests/2v1GeneralApplication_test.js
@@ -13,7 +13,7 @@ let parentCaseNumber, caseId, childCaseId, childCaseNumber, gaCaseReference;
 
 Feature('GA CCD 2v1 - General Application Journey @multiparty-e2e-tests @ui-nightly');
 
-Scenario('GA for 2v1 - Concurrent written representations journey @mm', async ({I, api}) => {
+Scenario('GA for 2v1 - Concurrent written representations - without notice to with notice journey', async ({I, api}) => {
   parentCaseNumber = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, claimantType);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, parentCaseNumber);
   await api.notifyClaimDetails(config.applicantSolicitorUser, parentCaseNumber);
@@ -78,5 +78,5 @@ Scenario('GA for 2v1 - Concurrent written representations journey @mm', async ({
 });
 
 AfterSuite(async ({api}) => {
-  // await api.cleanUp();
+  await api.cleanUp();
 });
