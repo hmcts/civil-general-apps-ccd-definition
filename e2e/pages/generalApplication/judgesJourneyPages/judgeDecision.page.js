baf22528ddfe0ea8dc82e7f2251af86078dd61b1
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-01-27 09:30:20 +0000

    CIV-6683: Automation task (#508)
    * CIV-6683 Fix Typo
    
    * Fix request more info preview order doc label
    
    * fix spell mistake for specific-access-ctsc
    
    * Testing 6772 and 6683
    
    * fix for e2e tests
    
    * increasing xui memory
    
    * increasing xui memory
    
    * reverting back to latest xui
    
    * decreasing xui memory
    
    * code cleanup
    
    Co-authored-by: karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
    Co-authored-by: Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>

--- e2e/pages/generalApplication/judgesJourneyPages/judgeDecision.page.js
+++ e2e/pages/generalApplication/judgesJourneyPages/judgeDecision.page.js
@@ -21,6 +21,19 @@ module.exports = {
       I.click(this.fields.judgeDecision.options[decision]);
     });
     await I.clickContinue();
+  },
+
+  async judgeMakeDecisionForWithoutNotice(decision) {
+    I.waitForElement(this.fields.judgeDecision.id);
+    I.seeInCurrentUrl('MAKE_DECISION/MAKE_DECISIONGAJudicialDecision');
+    await within(this.fields.judgeDecision.id, () => {
+      I.click(this.fields.judgeDecision.options[decision]);
+    });
+  },
+
+  async verifyWrittenRepErrorMessage() {
+    await I.click('Continue');
+    await I.see('The application needs to be uncloaked before requesting written representations');
   }
 };
 
