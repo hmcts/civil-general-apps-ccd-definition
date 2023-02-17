68f56f1ea1102703935cef2f6f391e4c68c54d0e
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-02-17 09:07:56 +0000

    Test CIV-6929

--- e2e/tests/ui_tests/multiparty-ga-tests/1v2_different_solicitor_GeneralApplication_test.js
+++ e2e/tests/ui_tests/multiparty-ga-tests/1v2_different_solicitor_GeneralApplication_test.js
@@ -173,7 +173,7 @@ Scenario('Without Notice application - Org2 Solicitor Initiate GA - Awaiting Wri
   await I.dontSee('Applications', 'div.mat-tab-label-content');
 }).retry(1);
 
-Scenario('With Notice application - Org3 Solicitor Initiate GA @e2e-tests', async ({api, I}) => {
+Scenario('With Notice application - Org3 Solicitor Initiate GA @e2e-tests @mm', async ({api, I}) => {
   civilCaseReference = await api.createUnspecifiedClaim(
     config.applicantSolicitorUser, mpScenario, 'Company');
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
