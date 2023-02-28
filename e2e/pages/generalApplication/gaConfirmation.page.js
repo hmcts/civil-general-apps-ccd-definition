e6b4acd0293242dfb74c7657e0529bf54d7b41ed
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2023-02-28 12:50:55 +0000

    CIV-7014 Modified message with field locators

--- e2e/pages/generalApplication/gaConfirmation.page.js
+++ e2e/pages/generalApplication/gaConfirmation.page.js
@@ -9,25 +9,25 @@ module.exports = {
     confirmation: {
       id: '#confirmation-body'
     },
-    applicationFeeLink: '#confirmation-body a'
+    applicationFeeLink: '#confirmation-body a',
+    feeTextInfo: '#confirmation-body p:nth-child(2)',
+    paymentNavigationInfo: '#confirmation-body p:nth-child(3)'
   },
 
   async verifyConfirmationPage(parentCaseId, consentCheck, notice) {
     let fee;
     if ('no' === consentCheck && 'yes' === notice) {
-      fee = '£275';
+      fee = '£275.00';
     } else {
-      fee = '£108';
+      fee = '£108.00';
     }
 
-    let confirmation_msg = `Your application fee of ${fee} is now due for payment.
-     Your application will not be reviewed by the court until this fee has been paid.`
+    let confirmation_msg = `Your application fee of ${fee} is now due for payment. Your application will not be reviewed by the court until this fee has been paid.`
 
     I.waitForElement(this.fields.confirmation.id);
     I.seeInCurrentUrl('INITIATE_GENERAL_APPLICATION/confirm');
-    I.see(confirmation_msg);
-    I.see('To pay this fee, you will need to open your application ' +
-      'from the Applications tab of this case listing.');
+    await I.seeTextEquals(confirmation_msg, this.fields.feeTextInfo);
+    I.seeTextEquals('To pay this fee, you will need to open your application from the Applications tab of this case listing.', this.fields.paymentNavigationInfo);
     I.seeTextEquals('Pay your application fee', this.fields.applicationFeeLink);
     await waitForFinishedBusinessProcess(parentCaseId, config.applicantSolicitorUser);
     await waitForGAFinishedBusinessProcess(parentCaseId, config.applicantSolicitorUser);
