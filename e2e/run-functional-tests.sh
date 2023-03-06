4647a0d349b266118a14d4e5776ce8e93842644c
Azam <muhammad.ghulam@hmcts.net>
2023-03-06 16:26:05 +0000

    Revert "CIV-7411 Test configuration for prod and non prod"
    This reverts commit fe78ed643fa168b065ea2b14c5e175cc86fe95a0.

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -3,5 +3,11 @@ set -ex
 
 echo "Running FE and API tests on tests on ${ENVIRONMENT} env"
 # this requires update later on when we need different tests to run on each environment. .for example enhancements test only on preview
-yarn test:master-e2e-tests
-yarn test:api
+
+if [ ${ENVIRONMENT} == preview ]; then
+  yarn test:non-prod-e2e-tests
+  yarn test:api
+else
+  yarn test:master-e2e-tests
+  yarn test:api
+fi
