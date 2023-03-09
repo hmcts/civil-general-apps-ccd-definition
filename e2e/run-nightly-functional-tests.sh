1bda080f53f0fdb169d07e704ea8d0669c875f07
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-03-09 13:59:31 +0000

    CIV-7508 removed API tests from nightly suite (#602)
    * removed API tests from nightly suite
    
    * Skipping all the HN tests

--- e2e/run-nightly-functional-tests.sh
+++ e2e/run-nightly-functional-tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ex
 
-echo "Running Nightly FE and API tests on ${ENVIRONMENT} env"
+echo "Running Nightly FE tests on ${ENVIRONMENT} env"
 if [ ${ENVIRONMENT} == demo ]; then
   yarn test:nightly-e2e-tests
-  yarn test:nightly-api-tests
 fi
