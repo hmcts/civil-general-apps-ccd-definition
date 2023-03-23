29f5a601efa52a31d9f98bade1e55a7b8b115994
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2023-03-23 08:28:11 +0000

    changes required

--- e2e/tests/api_spec_claim_test/api_spec_1v2_claimant_response_offline_nightly_test.js
+++ e2e/tests/api_spec_claim_test/api_spec_1v2_claimant_response_offline_nightly_test.js
@@ -78,7 +78,7 @@ Scenario('Case offline APPLICATION_DISMISSED', async ({api}) => {
   await api.verifyGAState(config.applicantSolicitorUser, civilCaseReference, gaCaseReference, 'APPLICATION_DISMISSED');
 });
 
-Scenario.only('Case offline AWAITING_RESPONDENT_RESPONSE', async ({api}) => {
+Scenario('Case offline AWAITING_RESPONDENT_RESPONSE', async ({api}) => {
   civilCaseReference = await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
   console.log('Civil Case created for general application: ' + civilCaseReference);
   console.log('Make a General Application');
