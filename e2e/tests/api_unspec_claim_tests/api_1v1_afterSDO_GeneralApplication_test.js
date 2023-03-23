daa6c8c9fe90bfb716a3666a5c4f1cfff7c6da9b
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2023-03-23 13:36:29 +0000

    CIV-4995 Added API tests for updating caseManagement Location after SDO

--- e2e/tests/api_unspec_claim_tests/api_1v1_afterSDO_GeneralApplication_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_afterSDO_GeneralApplication_test.js
@@ -8,7 +8,7 @@ let civilCaseReference, gaCaseReference;
 Feature('Unspec 1v1 - General Application after SDO Journey @api-tests');
 
 // Test before enable this test
-Scenario('Claimant create GA - JUDICIAL_REFERRAL state', async ({api, I}) => {
+Scenario('Claimant create GA - JUDICIAL_REFERRAL state @api-tests', async ({api, I}) => {
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', claimAmountJudge);
   await api.amendClaimDocuments(config.applicantSolicitorUser);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
@@ -28,7 +28,7 @@ Scenario('Claimant create GA - JUDICIAL_REFERRAL state', async ({api, I}) => {
     await api.judgeMakesDecisionOrderMade(config.judgeLocalUser, gaCaseReference);
   }
   await api.claimantResponseUnSpec(config.applicantSolicitorUser, mpScenario, 'JUDICIAL_REFERRAL');
-  await api.verifyGAState(config.applicantSolicitorUser, civilCaseReference, gaCaseReference, 'ORDER_MADE');
+  await api.verifyGALocation(config.applicantSolicitorUser, gaCaseReference, civilCaseReference);
 });
 
 AfterSuite(async ({api}) => {
