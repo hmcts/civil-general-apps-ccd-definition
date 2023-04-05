07862d0b7c18e3c14042791a5c9a35b914e838f7
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-04-05 10:18:58 +0100

    added 123 test

--- e2e/tests/ui_tests/multiparty-ga-tests/1v2_CP_ApplicationOrders_test.js
+++ e2e/tests/ui_tests/multiparty-ga-tests/1v2_CP_ApplicationOrders_test.js
@@ -6,7 +6,7 @@ let civilCaseReference, gaCaseReference;
 
 Feature('Before SDO 1v2 - GA CP - Applications Orders @ui-nightly');
 
-Scenario('1v2 - Assisted order - Without notice journey', async ({I, api}) => {
+Scenario('1v2 - Assisted order - Without notice journey @123', async ({I, api}) => {
   civilCaseReference = await api.createUnspecifiedClaim(
     config.applicantSolicitorUser, mpScenario, 'Company');
   await api.amendClaimDocuments(config.applicantSolicitorUser);
@@ -42,5 +42,5 @@ Scenario('1v2 - Assisted order - Without notice journey', async ({I, api}) => {
 });
 
 AfterSuite(async ({api}) => {
-  await api.cleanUp();
+  //await api.cleanUp();
 });
