a33e964058e465ab571e773c62a18132b51dd780
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2023-04-06 10:17:44 +0100

    CIV-3618 Revert test

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -ex
 
+echo "Running FE tests on tests on ${ENVIRONMENT} env"
+yarn test:master-e2e-tests
+
 echo "Running API tests on tests on ${ENVIRONMENT} env"
-yarn test:e2e-multiparty
+yarn test:api
