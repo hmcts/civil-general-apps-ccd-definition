ab4678d6ef753b6b9211e13a339bf9bd98feec59
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-04-12 15:22:05 +0100

    CIV-6851 test branch

--- e2e/tests/api_spec_claim_test/api_spec_1v2_move_case_to_caseman_offline_nightly_test.js
+++ e2e/tests/api_spec_claim_test/api_spec_1v2_move_case_to_caseman_offline_nightly_test.js
@@ -4,7 +4,7 @@ const config = require('../../config.js');
 let civilCaseReference,
     gaCaseReference;
 
-Feature('GA SPEC Claim 1v2 Move to Case Man Case Close API tests @api-offline-nightly @api-nightly');
+Feature('GA SPEC Claim 1v2 Move to Case Man Case Close API tests @api-tests');
 
 Scenario('Case offline APPLICATION_SUBMITTED_AWAITING_JUDICIAL_DECISION @api-tests', async ({api}) => {
   civilCaseReference = await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
