7b7e4ea1c0ae78ee6f560c5d791915a2b94344c0
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-05-19 14:27:21 +0100

    running @regression2

--- e2e/tests/ui_tests/multiparty-ga-tests/2v1GeneralApplication_test.js
+++ e2e/tests/ui_tests/multiparty-ga-tests/2v1GeneralApplication_test.js
@@ -11,7 +11,7 @@ const claimantType = 'Company';
 const awaitingPaymentStatus = states.AWAITING_APPLICATION_PAYMENT.name;
 let civilCaseReference, gaCaseReference, user;
 
-Feature('GA CCD 2v1 - General Application Journey @multiparty-e2e-tests @ui-nightly  @regression1');
+Feature('GA CCD 2v1 - General Application Journey @multiparty-e2e-tests @ui-nightly  @regression2');
 
 Scenario('GA for 2v1 - Concurrent written representations - without notice to with notice journey', async ({
                                                                                                              I,
