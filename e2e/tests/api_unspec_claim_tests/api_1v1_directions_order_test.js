b71d844a6e7cd804c0b18c4f9979c7dc50e9ed50
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2023-05-31 08:49:44 +0100

    Update api_1v1_directions_order_test.js

--- e2e/tests/api_unspec_claim_tests/api_1v1_directions_order_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_directions_order_test.js
@@ -6,7 +6,7 @@ let civilCaseReference, gaCaseReference;
 
 Feature('GA 1v1 Judge Make Order Directions Order API tests @api-tests');
 
-Scenario.skip('Judge makes decision 1V1 - VARY-JUDGEMENT - DIRECTIONS ORDER - Respondent upload Directions Document', async ({api}) => {
+Scenario.only('Judge makes decision 1V1 - VARY-JUDGEMENT - DIRECTIONS ORDER - Respondent upload Directions Document', async ({api}) => {
   civilCaseReference = await api.createUnspecifiedClaim(
     config.applicantSolicitorUser, mpScenario, 'Company');
   await api.amendClaimDocuments(config.applicantSolicitorUser);
