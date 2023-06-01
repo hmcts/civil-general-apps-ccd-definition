7912a2f6f47501cf54e5c5f25dd9c2dcf1f3b883
Azam <muhammad.ghulam@hmcts.net>
2023-06-01 11:49:44 +0100

    CIV-8533 updated test

--- e2e/tests/api_unspec_claim_tests/api_1v1_approve_consent_order_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_approve_consent_order_test.js
@@ -32,14 +32,8 @@ Scenario('caseworker makes decision 1V1 - CONSENT ORDER', async ({api}) => {
 });
 
 Scenario('Judge makes decision 1V1 - CONSENT ORDER - Uncloak Application', async ({api}) => {
-  civilCaseReference = await api.createUnspecifiedClaim(
-    config.applicantSolicitorUser, mpScenario, 'Company');
-  await api.amendClaimDocuments(config.applicantSolicitorUser);
-  await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
-  await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
-  console.log('Civil Case created for general application: ' + civilCaseReference);
 
-  console.log('Make a General Application');
+  console.log('Make a General Application for Consent order');
   gaCaseReference = await api.initiateConsentGeneralApplication(config.applicantSolicitorUser, civilCaseReference, false, false);
 
   console.log('*** Start response to GA Case Reference: ' + gaCaseReference + ' ***');
