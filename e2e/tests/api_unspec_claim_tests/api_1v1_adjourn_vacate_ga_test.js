748fa3250588dca14bad28f2bca7ddbab1a2720e
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-06-01 10:13:43 +0100

    CIV-7575 api test

--- e2e/tests/api_unspec_claim_tests/api_1v1_adjourn_vacate_ga_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_adjourn_vacate_ga_test.js
@@ -38,6 +38,7 @@ Scenario('AC 2 - 14 Days without consent, without notice', async ({api}) => {
   let hearingDate = await api.createDateString(14);
   civilCaseReference = await api.createUnspecifiedClaim(
     config.applicantSolicitorUser, mpScenario, 'Company');
+  await api.amendClaimDocuments(config.applicantSolicitorUser);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
   await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
   console.log('Civil Case created for general application: ' + civilCaseReference);
