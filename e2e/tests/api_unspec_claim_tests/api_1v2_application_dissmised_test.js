e8ecdeaedd7b6d0b3a3fa792c678a8a9c80b6afd
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2023-06-12 13:59:21 +0100

    CIV-9132 Revert test

--- e2e/tests/api_unspec_claim_tests/api_1v2_application_dissmised_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v2_application_dissmised_test.js
@@ -5,7 +5,7 @@ const {I} = inject();
 
 let civilCaseReference, gaCaseReference, gaCaseReferenceSolicitorOne, gaCaseReferenceSolicitorTwo;
 
-Feature('GA 1v2 Judge Dismiss Application API tests @withoutnotice1v2');
+Feature('GA 1v2 Judge Dismiss Application API tests @api-nightly');
 
 Scenario('1V2 Different Solicitors - Respondent Solicitors initiate Without Application', async ({api}) => {
   civilCaseReference = await api.createUnspecifiedClaim(
