e695d8c4f59296dda62d24c2fc9c4fbac8eb570a
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-06-28 09:50:57 +0100

    added clickTab methods

--- e2e/helpers/PuppeteerHelper.js
+++ e2e/helpers/PuppeteerHelper.js
@@ -7,9 +7,19 @@ class PuppeteerHelper extends Helper {
 
   async waitForNavigationToComplete(locator) {
     const page = this.helpers[helperName].page;
-    await page.waitForSelector(locator, {visible: true});
+    await page.waitForSelector(locator, 10);
     await page.click(locator);
     await page.waitForNavigation();
+  }
+
+  async clickTab(tabTitle) {
+    const page = this.helpers[helperName].page;
+    const tabXPath = `//div[text()='${tabTitle}']`;
+    const tabExists = !!(await page.waitForXPath(tabXPath, {timeout: 6000}));
+    if (tabExists) {
+      const clickableTab = await page.$x(tabXPath);
+      await page.evaluate(el => {return el.click();}, clickableTab[0]);
+    }
   }
 }
 module.exports = PuppeteerHelper;
--- e2e/helpers/WebDriverHelper.js
+++ e2e/helpers/WebDriverHelper.js
@@ -13,6 +13,27 @@ class WebDriverHelper extends Helper {
     }
     await helper.wait(webDriverWait);
   }
+
+  async clickTab(tabTitle) {
+    const helper = this.helpers.WebDriver;
+    const tabXPath = `//div[text()='${tabTitle}']`;
+    const elements = await helper._locateClickable(tabXPath);
+    const selector = elements[0].selector;
+
+    helper.executeScript(function (el) {
+      (
+        function(expression, parentElement) {
+          const r = [];
+          const x = document.evaluate(expression, parentElement ||
+            document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+          for (let i = 0, l = x.snapshotLength; i < l; i++) {
+            r.push(x.snapshotItem(i));
+          }
+          return r;
+        }
+      )(el)[0].click();
+    }, selector);
+  }
 }
 
 module.exports = WebDriverHelper;
