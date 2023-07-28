ff72bb737f0acc9e3c92bdcd39ddd3716e620b5d
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-07-28 11:13:36 +0100

    sprint demo test

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -2,7 +2,7 @@
 set -ex
 
 echo "Running FE tests on tests on ${ENVIRONMENT} env"
-yarn test:master-e2e-tests
+#yarn test:master-e2e-tests
 
 echo "Running API tests on tests on ${ENVIRONMENT} env"
-yarn test:api
+yarn test:e2e
