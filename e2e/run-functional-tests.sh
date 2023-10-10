dfa68fd21e00ccc5c34137fc3af522f1f6daea09
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2023-10-10 15:44:51 +0100

    changes to run only failed tests

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ex
 
-echo "Running FE tests on tests on ${ENVIRONMENT} env"
-yarn test:master-e2e-tests
+
 
 echo "Running API tests on tests on ${ENVIRONMENT} env"
 yarn test:api
