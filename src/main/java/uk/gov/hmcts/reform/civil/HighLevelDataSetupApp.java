c6f379b263f91e2ac40cec2eadb59b431c3a8808
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2023-10-17 11:54:28 +0100

    file name correction

--- src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/civil/HighLevelDataSetupApp.java
@@ -84,7 +84,7 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     @Override
     protected List<String> getAllDefinitionFilesToLoadAt(String definitionsPath) {
         String environmentName = environment.name().toLowerCase(Locale.UK);
-        return List.of(String.format("build/ccd-release-config/civil-ccd-%s.xlsx", environmentName));
+        return List.of(String.format("build/ccd-release-config/civil-ga-ccd-%s.xlsx", environmentName));
     }
 
     @Override
