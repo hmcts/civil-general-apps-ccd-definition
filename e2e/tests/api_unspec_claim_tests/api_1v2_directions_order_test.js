69ad7e8cf686d987e5696c29261b2e2f570d780e
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2023-11-07 12:04:04 +0000

    removed unwanted assertions

--- e2e/tests/api_unspec_claim_tests/api_1v2_directions_order_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v2_directions_order_test.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-unused-vars */
 const config = require('../../config.js');
 const mpScenario = 'ONE_V_TWO_TWO_LEGAL_REP';
-const genAppType = 'STAY_THE_CLAIM';
-
 let civilCaseReference, gaCaseReference;
 
 Feature('GA 1v2 Judge Make Order Directions Order API tests @api-nightly');
@@ -20,14 +18,12 @@ Scenario('Judge makes decision 1V2 - DIRECTIONS ORDER', async ({api}) => {
   console.log('*** Start response to GA Case Reference: ' + gaCaseReference + ' ***');
   await api.respondentResponse1v2(config.defendantSolicitorUser, config.secondDefendantSolicitorUser, gaCaseReference);
   console.log('*** End Response to GA Case Reference: ' + gaCaseReference + ' ***');
-  let state;
   console.log('*** Start Judge Directions Order on GA Case Reference: ' + gaCaseReference + ' ***');
   if (['preview', 'demo', 'aat'].includes(config.runningEnv)) {
-    state = await api.judgeMakesDecisionDirectionsOrder(config.judgeUser, gaCaseReference);
+    await api.judgeMakesDecisionDirectionsOrder(config.judgeUser, gaCaseReference);
   } else {
-    state = await api.judgeMakesDecisionDirectionsOrder(config.judgeLocalUser, gaCaseReference);
+    await api.judgeMakesDecisionDirectionsOrder(config.judgeLocalUser, gaCaseReference);
   }
-  await api.judgeRevisitScheduler(gaCaseReference, state, genAppType);
   console.log('*** End Judge Directions Order GA Case Reference: ' + gaCaseReference + ' ***');
 });
 
