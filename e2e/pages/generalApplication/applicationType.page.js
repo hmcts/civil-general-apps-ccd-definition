dbcd67300a785cd3d4c843751b9630fdf4b2c24f
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-01-03 16:46:13 +0000

    CIV-11935 CIV-11936 Content Changes - Make Application and Respondent (#996)
    * CIV-11935 Content Changes - Make Application
    
    * Revert list element code
    
    * Revert "Revert list element code"
    
    This reverts commit f49fe0c5302fd31eee4226922c1d22afeb522e53.
    
    * CIV-11936 Update Response journey content
    
    ---------
    
    Co-authored-by: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>

--- e2e/pages/generalApplication/applicationType.page.js
+++ e2e/pages/generalApplication/applicationType.page.js
@@ -29,7 +29,7 @@ module.exports = {
   async verifyAllApplicationTypes(applicationTypes, caseNumber) {
     I.waitForElement(this.fields.applicationType.id);
     I.seeInCurrentUrl('INITIATE_GENERAL_APPLICATIONGATypePage');
-    I.see('What type of applications do you want to make?');
+    I.see('What type of application(s) do you want to make?');
     I.see(caseNumber);
     applicationTypes.forEach(type => {
       return I.see(type);
@@ -44,7 +44,7 @@ module.exports = {
     });
     await I.click('Continue');
     await I.waitForInvisible(locate('div.spinner-container').withText('Loading'), 20);
-    await I.see('It is not possible to select an additional application type when applying to vary judgment');
+    await I.see('It is not possible to select an additional application type when applying to vary payment terms of judgment');
     await I.click('Cancel');
     await I.waitForElement(this.fields.eventDropdown, 5);
   }
