ef8f5388ac597e80ba7f69edab4264e0cd28b26b
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2024-01-19 10:58:34 +0000

    updated test:regression-e2e-tests

--- e2e/run-nightly-functional-tests.sh
+++ e2e/run-nightly-functional-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-echo "Running Nightly FE tests on ${ENVIRONMENT} env"
+echo "Running FE regression tests on ${ENVIRONMENT} env"
 if [ ${ENVIRONMENT} == demo ]; then
-  yarn test:nightly-e2e-tests
+  yarn test:regression-e2e-tests
 fi
