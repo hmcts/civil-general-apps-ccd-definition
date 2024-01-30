bd34f6b80bc0537e66a5efdb36e473812a3a8eaf
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-01-30 09:49:30 +0000

    CIV-12192 Fix api nightly (#1035)
    * CIV-12192 Fix api nightly
    
    * Remove tag 1
    
    * CIV-12192 Modify tag
    
    * CIV-12192 Update api tags
    
    * fix for cross browser test
    
    * removed 888 tag
    
    ---------
    
    Co-authored-by: Madhan Mahadevan <madhan.mahadevan@hmcts.net>

--- e2e/tests/api_unspec_claim_tests/api_1v2_general_app_collections_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v2_general_app_collections_test.js
@@ -4,10 +4,10 @@ const mpScenario = 'ONE_V_TWO_TWO_LEGAL_REP';
 
 let civilCaseReference, gaCaseReference;
 
-Feature('GA 1v2 application collection for different solicitor API tests @api-nightly');
+Feature('GA 1v2 application collection for different solicitor API tests @api-tests');
 
 
-Scenario('GA 1v2  - Without Notice Application Collection After Judge Makes Decision List for Hearing  @api-tests', async ({api}) => {
+Scenario('GA 1v2  - Without Notice Application Collection After Judge Makes Decision List for Hearing', async ({api}) => {
 
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser,
     mpScenario, 'SoleTrader', '11000');
@@ -44,7 +44,7 @@ Scenario('GA 1v2  - Without Notice Application Collection After Judge Makes Deci
 
 });
 
-Scenario('GA 1v2  - Without Notice Application Collection after Creation of GA Case Test  @api-tests', async ({api}) => {
+Scenario('GA 1v2  - Without Notice Application Collection after Creation of GA Case Test', async ({api}) => {
 
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser,
     mpScenario, 'SoleTrader', '11000');
