b9fff0e9ff995a46b55df47b163ab2142696dee1
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-01-30 10:09:12 +0000

    Add wait

--- e2e/tests/api_unspec_claim_tests/api_1v1_judge_unless_order_decision_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_judge_unless_order_decision_test.js
@@ -32,9 +32,10 @@ Scenario('Judge makes decision 1V1 - unless order  @api-tests @api-scheduler-tes
   console.log('*** End Judge makes decision unless order - GA Case Reference: ' + gaCaseReference + ' ***');
 });
 
-Scenario('Judge Revisit 1V1 - unless order End Date Scheduler @api-scheduler-test', async ({api}) => {
+Scenario('Judge Revisit 1V1 - unless order End Date Scheduler @api-scheduler-test', async ({api, I}) => {
 
   console.log('*** Triggering Judge Revisit unless order Scheduler ***');
+  await I.wait(30);
   await api.judgeRevisitScheduler(gaCaseReference, state, genAppType);
   console.log('*** End of Judge Revisit unless order Scheduler ***');
 
