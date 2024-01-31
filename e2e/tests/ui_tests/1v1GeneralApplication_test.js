0891dea2dd9f5637eb3f1833dce07e40682ec801
Madhan Mahadevan <madhan.mahadevan@hmcts.net>
2024-01-31 14:19:09 +0000

    Revert "Skipped tests due to CIV-12514"
    This reverts commit 70a5607f8727e54adbd2442b57225c574e1e993a.

--- e2e/tests/ui_tests/1v1GeneralApplication_test.js
+++ e2e/tests/ui_tests/1v1GeneralApplication_test.js
@@ -176,8 +176,7 @@ Scenario.skip('GA for 1v1 Specified Claim- Dismissal order journey @regression2'
   await api.assertGaAppCollectionVisiblityToUser(config.defendantSolicitorUser, civilCaseReference, gaCaseReference, 'Y');
 });
 
-// Skipped due to CIV-12514
-Scenario.skip('GA for 1v1- respond to application - Request more information @regression2', async ({I, api}) => {
+Scenario('GA for 1v1- respond to application - Request more information @regression2', async ({I, api}) => {
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', '11000');
   await api.amendClaimDocuments(config.applicantSolicitorUser);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
