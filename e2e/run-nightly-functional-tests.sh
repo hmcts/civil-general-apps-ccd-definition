7d8c46f1745805a5647570a128d07cdda975041f
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2024-02-07 11:48:18 +0000

    update

--- e2e/run-nightly-functional-tests.sh
+++ e2e/run-nightly-functional-tests.sh
@@ -2,7 +2,7 @@
 set -ex
 
 echo "Running FE regression tests on ${ENVIRONMENT} env"
-yarn playwright install
 if [ ${ENVIRONMENT} == demo ]; then
+  yarn playwright-install
   yarn test:regression-e2e-tests
 fi
