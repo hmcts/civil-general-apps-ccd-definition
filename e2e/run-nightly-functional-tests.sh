804406c00e8d0836360f16ead43e938500b1ea03
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2024-04-24 19:57:33 +0100

    Update run-nightly-functional-tests.sh

--- e2e/run-nightly-functional-tests.sh
+++ e2e/run-nightly-functional-tests.sh
@@ -2,7 +2,5 @@
 set -ex
 
 echo "Running FE regression tests on ${ENVIRONMENT} env"
-if [ ${ENVIRONMENT} == aat ]; then
-  yarn playwright-install
-  yarn test:regression-e2e-tests
-fi
+yarn playwright-install
+yarn test:regression-e2e-tests
