4493b369c1c3dbfb80aea2d93d772366252e34a3
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-04-25 15:53:41 +0100

    run lr vs lip

--- e2e/tests/ga_for_lip_test/api_LRvLIP_WithOutNotice.js
+++ e2e/tests/ga_for_lip_test/api_LRvLIP_WithOutNotice.js
@@ -3,11 +3,10 @@ const config = require('../../config.js');
 let civilCaseReference, gaCaseReference;
 const mpScenario = 'ONE_V_ONE';
 const claimAmountJudge = '11000';
-const errorMsg = 'Sorry this service is not available in the current case management location, please raise an application manually.';
 
-Feature('General Application LR vs LIP 1V1 @ga-smoke-tests');
+Feature('General Application LR vs LIP 1V1 @lrvslip ');
 
-Scenario('GA 1v1  - Judge Makes Decision Order Made @smoke-tests @lrvslip', async ({api, I}) => {
+Scenario('GA 1v1  - Judge Makes Decision Order Made @lrvslip ', async ({api, I}) => {
 
   civilCaseReference = await api.createClaimWithRespondentLitigantInPerson(config.applicantSolicitorUser, mpScenario);
   await api.notifyClaimLip(config.applicantSolicitorUser);
@@ -16,18 +15,6 @@ Scenario('GA 1v1  - Judge Makes Decision Order Made @smoke-tests @lrvslip', asyn
   console.log('Civil Case created for general application: ' + civilCaseReference);
   console.log('Make a General Application');
   gaCaseReference = await api.initiateGeneralApplicationWithOutNotice(config.applicantSolicitorUser, civilCaseReference);
-  /*console.log('Without Notice General Application Initiated by Defendant2 : ' + gaCaseReference);
-  console.log('*** Start Judge makes decision order made: ' + gaCaseReference + ' ***');
-  if(['preview', 'demo', 'aat'].includes(config.runningEnv)) {
-    await api.judgeMakesDecisionOrderMade(config.judgeUser2WithRegionId2, gaCaseReference);
-  }else {
-    await api.judgeMakesDecisionOrderMade(config.judgeLocalUser, gaCaseReference);
-  }
-  console.log('*** End Judge makes decision order made - GA Case Reference: ' + gaCaseReference + ' ***');
-
-  await I.login(config.defendantSolicitorUser);
-  await I.navigateToTab(civilCaseReference, 'Applications');
-  await I.see('Order Made');*/
 });
 
 AfterSuite(async ({api}) => {
