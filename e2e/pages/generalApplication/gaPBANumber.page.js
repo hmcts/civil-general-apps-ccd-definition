c56f5b4fcbc06032003f2bc495d417e632dc0c24
mfallonhmcts <114912573+mfallonhmcts@users.noreply.github.com>
2024-05-03 15:57:19 +0100

    DTSCCI-258 - Put correct claim fee amounts in General Applications API and UI Tests

--- e2e/pages/generalApplication/gaPBANumber.page.js
+++ e2e/pages/generalApplication/gaPBANumber.page.js
@@ -15,11 +15,11 @@ module.exports = {
     I.see('You will be able to pay for your application once it has been submitted.');
     I.see('Application fee to pay');
     if (('no' === consentCheck && 'yes' === notice) && ('Vary payment terms of judgment' !== appType || 'Vary order' !== appType)) {
-      I.see('£275.00');
+      I.see('£303.00');
     } else if ('Vary payment terms of judgment' === appType || 'Vary order' === appType) {
-      I.see('£14.00');
+      I.see('£15.00');
     } else {
-      I.see('£108.00');
+      I.see('£119.00');
     }
     await I.clickContinue();
   },
