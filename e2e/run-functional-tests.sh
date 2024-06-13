2c36cdeb44408ee7ab63286bdb907a5ca4440730
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2024-06-13 15:52:09 +0100

    changing order of tests

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -5,10 +5,10 @@ export CCD_UI_TESTS=true
 if [ ${ENVIRONMENT} == preview ]; then
   echo "Running FE tests on tests on ${ENVIRONMENT} env"
   yarn test:master-e2e-tests
-  echo "Running API tests on tests on ${ENVIRONMENT} env"
-  yarn test:api
   echo "Running non prod tests on tests on ${ENVIRONMENT} env"
   yarn test:apie2e-nonprod
+  echo "Running API tests on tests on ${ENVIRONMENT} env"
+  yarn test:api
 else
   echo "Running FE tests on tests on ${ENVIRONMENT} env"
   yarn test:master-e2e-tests
