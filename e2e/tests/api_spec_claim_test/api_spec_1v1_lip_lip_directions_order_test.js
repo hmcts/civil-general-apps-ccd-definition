c0b1e47d8f3f89768029ddaffab2c4088ec05293
jeswanth <jeswanth.reddy@hmcts.net>
2024-06-25 14:56:23 +0100

    removed the tags

--- e2e/tests/api_spec_claim_test/api_spec_1v1_lip_lip_directions_order_test.js
+++ e2e/tests/api_spec_claim_test/api_spec_1v1_lip_lip_directions_order_test.js
@@ -9,14 +9,14 @@ Before(async () => {
   await createAccount(config.defendantCitizenUser2.email, config.defendantCitizenUser2.password);
 });
 
-Scenario('Spec Claimant create GA with single application type - @galip', async ({ api }) => {
+Scenario('Spec Claimant create GA with single application type', async ({ api }) => {
   civilCaseReference = await api.createClaimWithUnrepresentedClaimant(config.applicantCitizenUser, 'SmallClaims', false, 'INDIVIDUAL');
   console.log(civilCaseReference);
   const { gaCaseState } = await api.createGAApplicationWithUnrepresented(config.applicantCitizenUser, civilCaseReference);
   assert.equal(gaCaseState, 'AWAITING_APPLICATION_PAYMENT');
 })
 
-Scenario('Spec Claimant create GA with multiple application types - @galip', async ({ api }) => {
+Scenario('Spec Claimant create GA with multiple application types', async ({ api }) => {
   civilCaseReference = await api.createClaimWithUnrepresentedClaimant(config.applicantCitizenUser, 'SmallClaims', false, 'INDIVIDUAL');
   const { gaCaseState } = await api.createGAApplicationWithUnrepresented(config.applicantCitizenUser, civilCaseReference, typeOfApplication = 'multiple');
   assert.equal(gaCaseState, 'AWAITING_APPLICATION_PAYMENT');
