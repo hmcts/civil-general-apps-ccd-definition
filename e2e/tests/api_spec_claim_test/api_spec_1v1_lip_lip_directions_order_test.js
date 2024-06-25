1dd67799e760c71b1206521cdd9863c1c9a863f4
jeswanth <jeswanth.reddy@hmcts.net>
2024-06-25 14:53:11 +0100

    aaded for GA api test

---
+++ e2e/tests/api_spec_claim_test/api_spec_1v1_lip_lip_directions_order_test.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+const config = require('../../config.js');
+const { assert } = require('chai');;
+let civilCaseReference;
+
+Feature('Create Lip v Lip claim -  Default Judgment');
+
+Before(async () => {
+  await createAccount(config.defendantCitizenUser2.email, config.defendantCitizenUser2.password);
+});
+
+Scenario('Spec Claimant create GA with single application type - @galip', async ({ api }) => {
+  civilCaseReference = await api.createClaimWithUnrepresentedClaimant(config.applicantCitizenUser, 'SmallClaims', false, 'INDIVIDUAL');
+  console.log(civilCaseReference);
+  const { gaCaseState } = await api.createGAApplicationWithUnrepresented(config.applicantCitizenUser, civilCaseReference);
+  assert.equal(gaCaseState, 'AWAITING_APPLICATION_PAYMENT');
+})
+
+Scenario('Spec Claimant create GA with multiple application types - @galip', async ({ api }) => {
+  civilCaseReference = await api.createClaimWithUnrepresentedClaimant(config.applicantCitizenUser, 'SmallClaims', false, 'INDIVIDUAL');
+  const { gaCaseState } = await api.createGAApplicationWithUnrepresented(config.applicantCitizenUser, civilCaseReference, typeOfApplication = 'multiple');
+  assert.equal(gaCaseState, 'AWAITING_APPLICATION_PAYMENT');
+})
