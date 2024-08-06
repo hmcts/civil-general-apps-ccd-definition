26c491d51d7e72b35ba43781f3de77a4461c12d6
Mark Drummond <mark.drummond@version1.com>
2024-08-06 12:15:23 +0100

    use reg 2 judge

--- e2e/tests/api_unspec_claim_tests/api_1v1_afterSDO_GeneralApplication_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_afterSDO_GeneralApplication_test.js
@@ -7,7 +7,7 @@ let civilCaseReference, gaCaseReference;
 
 Feature('Unspec 1v1 - General Application after SDO Journey @api-nonprod');
 
-Scenario('Claimant create GA - JUDICIAL_REFERRAL state', async ({api, I}) => {
+Scenario('Claimant create GA - JUDICIAL_REFERRAL state @TEST1', async ({api, I}) => {
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', claimAmountJudge);
   await api.amendClaimDocuments(config.applicantSolicitorUser);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
@@ -22,7 +22,7 @@ Scenario('Claimant create GA - JUDICIAL_REFERRAL state', async ({api, I}) => {
 
   console.log('*** Start Judge makes decision order made: ' + gaCaseReference + ' ***');
   if (['preview', 'demo', 'aat'].includes(config.runningEnv)) {
-    await api.judgeMakesDecisionOrderMade(config.judgeUser, gaCaseReference);
+    await api.judgeMakesDecisionOrderMade(config.judgeUser2WithRegionId2, gaCaseReference);
   } else {
     await api.judgeMakesDecisionOrderMade(config.judgeLocalUser, gaCaseReference);
   }
