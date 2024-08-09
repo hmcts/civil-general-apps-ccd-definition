f4631dad5a852c61ec0044f7d0e6ad25f6fb464c
Mark Drummond <mark.drummond@version1.com>
2024-08-09 15:25:10 +0100

    Update nightly tests

--- e2e/tests/api_spec_claim_test/api_spec_1v2_move_case_to_caseman_offline_nightly_test.js
+++ e2e/tests/api_spec_claim_test/api_spec_1v2_move_case_to_caseman_offline_nightly_test.js
@@ -4,7 +4,7 @@ const config = require('../../config.js');
 let civilCaseReference,
   gaCaseReference;
 
-Feature('GA SPEC Claim 1v2 Move to Case Man Case Close API tests @api-offline-nightly @api-nightly  @TEST1');
+Feature('GA SPEC Claim 1v2 Move to Case Man Case Close API tests @api-offline-nightly @api-nightly @TEST1 ');
 
 Scenario('Case offline APPLICATION_SUBMITTED_AWAITING_JUDICIAL_DECISION', async ({api}) => {
   civilCaseReference = await api.createClaimWithRepresentedRespondent(config.applicantSolicitorUser, 'ONE_V_TWO_SAME_SOL');
