cfe67ec9bae04b8981fb31b1eebcc5cc0efb0f79
Mark Drummond <mark.drummond@version1.com>
2024-08-13 14:40:46 +0100

    testing nightly failure

--- e2e/tests/ui_tests/multiparty-ga-tests/2v1GeneralApplication_test.js
+++ e2e/tests/ui_tests/multiparty-ga-tests/2v1GeneralApplication_test.js
@@ -11,9 +11,9 @@ const claimantType = 'Company';
 const awaitingPaymentStatus = states.AWAITING_APPLICATION_PAYMENT.name;
 let civilCaseReference, gaCaseReference, user;
 
-Feature('GA CCD 2v1 - General Application Journey @multiparty-e2e-tests @ui-nightly @regression2');
+Feature('GA CCD 2v1 - General Application Journey @api-nightly');
 
-Scenario.only('GA for 2v1 - Concurrent written representations - without notice to with notice journey',
+Scenario.only('GA for 2v1 - Concurrent written representations - without notice to with notice  @api-nightly',
   async ({I, api}) => {
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, claimantType);
   await api.amendClaimDocuments(config.applicantSolicitorUser);
