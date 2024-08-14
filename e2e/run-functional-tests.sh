e19d19cd79b4af8322598f7e069f7ec240c2421e
Mark Drummond <mark.drummond@version1.com>
2024-08-14 14:36:31 +0100

    fix nightly test 2v1 ga

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -2,7 +2,7 @@
 set -ex
 
 export CCD_UI_TESTS=true
-if [ ${ENVIRONMENT} == aat ]; then
+if [ ${ENVIRONMENT} == preview ]; then
   echo "Running FE tests on tests on ${ENVIRONMENT} env"
   yarn test:master-e2e-tests
   echo "Running non prod tests on tests on ${ENVIRONMENT} env"
@@ -10,6 +10,8 @@ if [ ${ENVIRONMENT} == aat ]; then
   echo "Running API tests on tests on ${ENVIRONMENT} env"
   yarn test:api
 else
-  echo "Running FE regression tests on ${ENVIRONMENT} env"
-  yarn test:regression-e2e-tests
+  echo "Running FE tests on tests on ${ENVIRONMENT} env"
+  yarn test:master-e2e-tests
+  echo "Running API tests on tests on ${ENVIRONMENT} env"
+  yarn test:api
 fi
