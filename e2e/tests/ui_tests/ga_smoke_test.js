58d75774f19bed02315748cd6c764441a15dcc6b
drummondjm <93932689+drummondjm@users.noreply.github.com>
2024-09-18 15:50:35 +0100

    Remove test birmingham has no access to GA, as now live (#1261)

--- e2e/tests/ui_tests/ga_smoke_test.js
+++ e2e/tests/ui_tests/ga_smoke_test.js
@@ -3,7 +3,6 @@ const config = require('../../config.js');
 let civilCaseReference, gaCaseReference;
 const mpScenario = 'ONE_V_ONE';
 const claimAmountJudge = '11000';
-const errorMsg = 'Sorry this service is not available in the current case management location, please raise an application manually.';
 
 Feature('General Application Smoke tests @ga-smoke-tests');
 
@@ -30,23 +29,6 @@ Scenario('GA 1v1  - Judge Makes Decision Order Made @smoke-tests', async ({api, 
   await I.login(config.defendantSolicitorUser);
   await I.navigateToTab(civilCaseReference, 'Applications');
   await I.see('Order Made');
-});
-
-//this test will be removed after we do national rollout
-Scenario('Non EA Region Cases should not have access to the GA Feature @smoke-tests', async ({I, api}) => {
-  civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario,
-    'Company', '11000', true);
-  await api.amendClaimDocuments(config.applicantSolicitorUser);
-  await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
-  await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
-  await api.acknowledgeClaim(config.defendantSolicitorUser, civilCaseReference, true);
-  await api.defendantResponseClaim(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
-  await api.claimantResponseUnSpec(config.applicantSolicitorUser, mpScenario, 'JUDICIAL_REFERRAL');
-  await I.login(config.applicantSolicitorUser);
-  await I.navigateToCaseDetails(civilCaseReference);
-  if(['aat'].includes(config.runningEnv)) {
-    await I.verifyGAAccessToNonEARegion(errorMsg);
-  }
 });
 
 AfterSuite(async ({api}) => {
