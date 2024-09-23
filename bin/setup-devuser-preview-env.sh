33d8591cfc1c80ee8c68c7e13ecebde0b844d3d3
Mike Lemos <gobii@me.com>
2024-09-23 15:59:35 +0100

    DTSCCI-814 adding scripts for dev env in preview

--- bin/setup-devuser-preview-env.sh
+++ bin/setup-devuser-preview-env.sh
@@ -11,14 +11,17 @@ echo "Importing Camunda definitions"
 ./bin/pull-latest-camunda-files.sh ${camundaBranch}
 echo "Importing CCD definitions"
 ./bin/import-ccd-definition.sh "-e *-prod.json,*-shuttered.json" ${ccdBranch}
+rm -rf $(pwd)/build
+
+echo "Importing GA CCD definitions"
 ./bin/build-release-ccd-definition.sh preview
 ccdDefinitionFilePath="$(pwd)/build/ccd-release-config/civil-ccd-preview.xlsx"
 ./bin/utils/ccd-import-definition.sh ${ccdDefinitionFilePath}
 
+echo "Importing Camunda definitions"
 ./bin/pull-latest-dmn-files.sh ${dmnBranch}
 ./bin/pull-latest-camunda-wa-files.sh ${waStandaloneBranch}
 
-rm -rf $(pwd)/build/ccd-release-config
 rm -rf $(pwd)/camunda
 
 echo "ENV variables set for devuser-preview environment."
