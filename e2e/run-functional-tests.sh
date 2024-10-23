6fd9c9260f07253e3102d1dedd4b86a1a9820d78
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-10-23 11:16:08 +0100

    Run multiparty and nightly

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -4,11 +4,9 @@ set -ex
 export CCD_UI_TESTS=true
 if [ ${ENVIRONMENT} == preview ]; then
   echo "Running FE tests on tests on ${ENVIRONMENT} env"
-  yarn test:master-e2e-tests
+  yarn test:e2e-multiparty
   echo "Running non prod tests on tests on ${ENVIRONMENT} env"
-  yarn test:apie2e-nonprod
-  echo "Running API tests on tests on ${ENVIRONMENT} env"
-  yarn test:api
+  yarn test:nightly-api-tests
 else
   echo "Running FE tests on tests on ${ENVIRONMENT} env"
   yarn test:master-e2e-tests
