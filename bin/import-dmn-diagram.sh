3801dc6b08b035d071f224a1eec1d6d4681e4306
GarethLancaster <31533575+Gareth40342@users.noreply.github.com>
2024-12-06 14:31:50 +0000

    WIP

--- bin/import-dmn-diagram.sh
+++ bin/import-dmn-diagram.sh
@@ -18,7 +18,8 @@ dmnFilepath="$(realpath $workspace)/resources"
 
 dmnUploadErrors=0
 
-for file in $(find ${dmnFilepath} -name '*.dmn')
+echo "Uploading prod DMNs..."
+for file in $(find ${dmnFilepath} -name '*.dmn' -not -name '*-nonprod.dmn')
 do
   uploadResponse=$(curl --insecure -v --silent -w "\n%{http_code}" --show-error -X POST \
     ${CAMUNDA_BASE_URL:-http://localhost:9404}/engine-rest/deployment/create \
@@ -47,6 +48,36 @@ done
 if [[ "${dmnUploadErrors}" == "1" ]]; then
   echo "ERROR: one or more DMN uploads failed."
   exit 1
+fi
+
+echo "checking if there are any non prod dmns to upload for env: ${env} and then uploading it"
+
+if [ "${env}" == "preview" ] || [ "${env}" == "demo" ] || [ "${env}" == "perftest" ] || [ "${env}" == "ithc" ]; then
+echo "Uploading non-prod DMNs..."
+for file in $(find ${dmnFilepath} -name '*-nonprod.dmn')
+do
+  uploadResponse=$(curl --insecure -v --silent -w "\n%{http_code}" --show-error -X POST \
+    ${CAMUNDA_BASE_URL:-http://localhost:9404}/engine-rest/deployment/create \
+    -H "Accept: application/json" \
+    -H "ServiceAuthorization: Bearer ${serviceToken}" \
+    -F "deployment-name=$(basename ${file})" \
+    -F "deploy-changed-only=true" \
+    -F "deployment-source=$product" \
+    ${tenant_id:+'-F' "tenant-id=$tenant_id"} \
+    -F "file=@${dmnFilepath}/$(basename ${file})")
+
+upload_http_code=$(echo "$uploadResponse" | tail -n1)
+upload_response_content=$(echo "$uploadResponse" | sed '$d')
+
+if [[ "${upload_http_code}" == '200' ]]; then
+  echo "$(basename ${file}) non prod diagram uploaded successfully (${upload_response_content})"
+  continue;
+fi
+
+echo "$(basename ${file}) upload failed with http code ${upload_http_code} and response (${upload_response_content})"
+continue;
+
+done
 fi
 
 exit 0
