bd494afdd95ee3d990e057b936f555ed69fd2ec7
jeswanth <jeswanth.reddy@hmcts.net>
2024-12-17 14:51:58 +0000

    CIV-0000 skiping the test

--- e2e/api/steps.js
+++ e2e/api/steps.js
@@ -2822,7 +2822,7 @@ const assertNullGaDocVisibilityToUser = async ( user, parentCaseId, doc) => {
   else{
     docCivil = civilCaseData[doc + 'DocStaff'];
   }
-  console.log('document details')
+  console.log('document details');
   console.log(JSON.stringify(docCivil));
   assert.equal(typeof(docCivil), 'undefined');
 };
