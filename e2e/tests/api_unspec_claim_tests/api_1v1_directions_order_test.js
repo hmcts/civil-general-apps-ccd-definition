b5b75febcdd552f131d77277351a67befa8ecbee
rishisharma <rishi.sharma@hmcts.net>
2025-03-03 15:23:24 +0000

    DTSCCI-1721 bump service chart

--- e2e/tests/api_unspec_claim_tests/api_1v1_directions_order_test.js
+++ e2e/tests/api_unspec_claim_tests/api_1v1_directions_order_test.js
@@ -6,7 +6,7 @@ let civilCaseReference, gaCaseReference;
 
 Feature('GA 1v1 Judge Make Order Directions Order API tests @api-tests');
 
-Scenario.skip('Judge makes decision 1V1 - VARY-JUDGEMENT - DIRECTIONS ORDER - Respondent upload Directions Document', async ({api}) => {
+Scenario('Judge makes decision 1V1 - VARY-JUDGEMENT - DIRECTIONS ORDER - Respondent upload Directions Document', async ({api}) => {
 
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', '11000');
   await api.amendClaimDocuments(config.applicantSolicitorUser);
@@ -38,7 +38,7 @@ Scenario.skip('Judge makes decision 1V1 - VARY-JUDGEMENT - DIRECTIONS ORDER - Re
   await api.assertDocumentVisibilityToUser(config.applicantSolicitorUser, 'Claimant', civilCaseReference, gaCaseReference, doc);
 });
 
-Scenario.skip('Judge makes decision 1V1 - VARY-JUDGEMENT  as DEFENDANT - PROCEEDS IN HERITAGE', async ({api}) => {
+Scenario('Judge makes decision 1V1 - VARY-JUDGEMENT  as DEFENDANT - PROCEEDS IN HERITAGE', async ({api}) => {
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', '11000');
   await api.amendClaimDocuments(config.applicantSolicitorUser);
   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
--- e2e/tests/ui_tests/cp_tests/api_1v1_initiate_general_application_test.js
+++ e2e/tests/ui_tests/cp_tests/api_1v1_initiate_general_application_test.js
@@ -7,7 +7,7 @@ let civilCaseReference, gaCaseReference;
 
 Feature('1v1 unspecified assert general application unavailable before respondent assigned @e2e-tests');
 
-Scenario.skip('1v1 unspecified assert general application unavailable before respondent assigned @e2e-tests', async ({I,api}) => {
+Scenario('1v1 unspecified assert general application unavailable before respondent assigned @e2e-tests', async ({I,api}) => {
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', '11000');
   await api.amendClaimDocuments(config.applicantSolicitorUser);
   console.log('Assert Make a General Application fails, as respondent not assigned');
@@ -21,7 +21,7 @@ Scenario.skip('1v1 unspecified assert general application unavailable before res
 
 });
 
-Scenario.skip('1v1 specified assert general application unavailable before respondent assigned @e2e-tests', async ({I,api}) => {
+Scenario('1v1 specified assert general application unavailable before respondent assigned @e2e-tests', async ({I,api}) => {
   civilCaseReference = await api.createSpecifiedClaim(config.applicantSolicitorUser, mpScenario, false);
   console.log('Assert Make a General Application fails, as respondent not assigned');
   await I.login(config.applicantSolicitorUser);
