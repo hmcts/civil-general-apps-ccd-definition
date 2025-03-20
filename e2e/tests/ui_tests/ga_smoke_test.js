13f7e750ac20ed3d06a0cf29716d631e4435170c
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-03-20 09:54:16 +0000

    update

--- e2e/tests/ui_tests/ga_smoke_test.js
+++ e2e/tests/ui_tests/ga_smoke_test.js
@@ -6,31 +6,31 @@ const claimAmountJudge = '11000';
 
 Feature('General Application Smoke tests @ga-smoke-tests');
 
-// Scenario('GA 1v1  - Judge Makes Decision Order Made @smoke-tests', async ({api, I}) => {
-//   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', claimAmountJudge);
-//   await api.amendClaimDocuments(config.applicantSolicitorUser);
-//   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
-//   await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
-//   await api.acknowledgeClaim(config.defendantSolicitorUser, civilCaseReference, true);
-//   await api.defendantResponseClaim(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
-//   await api.claimantResponseUnSpec(config.applicantSolicitorUser, mpScenario, 'JUDICIAL_REFERRAL');
-//   console.log('Civil Case created for general application: ' + civilCaseReference);
-//   console.log('Make a General Application');
-//   gaCaseReference = await api.initiateGeneralApplicationWithOutNotice(config.defendantSolicitorUser, civilCaseReference);
-  // console.log('Without Notice General Application Initiated by Defendant2 : ' + gaCaseReference);
-  // console.log('*** Start Judge makes decision order made: ' + gaCaseReference + ' ***');
-  // if(['preview', 'demo', 'aat'].includes(config.runningEnv)) {
-  //   await api.judgeMakesDecisionOrderMade(config.judgeUser2WithRegionId2, gaCaseReference);
-  // }else {
-  //   await api.judgeMakesDecisionOrderMade(config.judgeLocalUser, gaCaseReference);
-  // }
-  // console.log('*** End Judge makes decision order made - GA Case Reference: ' + gaCaseReference + ' ***');
+Scenario('GA 1v1  - Judge Makes Decision Order Made @smoke-tests', async ({api, I}) => {
+  civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', claimAmountJudge);
+  await api.amendClaimDocuments(config.applicantSolicitorUser);
+  await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
+  await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
+  await api.acknowledgeClaim(config.defendantSolicitorUser, civilCaseReference, true);
+  await api.defendantResponseClaim(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
+  await api.claimantResponseUnSpec(config.applicantSolicitorUser, mpScenario, 'JUDICIAL_REFERRAL');
+  console.log('Civil Case created for general application: ' + civilCaseReference);
+  console.log('Make a General Application');
+  gaCaseReference = await api.initiateGeneralApplicationWithOutNotice(config.defendantSolicitorUser, civilCaseReference);
+  console.log('Without Notice General Application Initiated by Defendant2 : ' + gaCaseReference);
+  console.log('*** Start Judge makes decision order made: ' + gaCaseReference + ' ***');
+  if(['preview', 'demo', 'aat'].includes(config.runningEnv)) {
+    await api.judgeMakesDecisionOrderMade(config.judgeUser2WithRegionId2, gaCaseReference);
+  }else {
+    await api.judgeMakesDecisionOrderMade(config.judgeLocalUser, gaCaseReference);
+  }
+  console.log('*** End Judge makes decision order made - GA Case Reference: ' + gaCaseReference + ' ***');
 
-  // await I.login(config.defendantSolicitorUser);
-  // await I.navigateToTab(civilCaseReference, 'Applications');
-  // await I.see('Order Made');
-// });
-
-AfterSuite(async ({api}) => {
-  await api.cleanUp();
+  await I.login(config.defendantSolicitorUser);
+  await I.navigateToTab(civilCaseReference, 'Applications');
+  await I.see('Order Made');
 });
+
+// AfterSuite(async ({api}) => {
+//   await api.cleanUp();
+// });
