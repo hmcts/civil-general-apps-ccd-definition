f2bfa612648e76e241779bc23b9264243c1f4557
kalachandrasekar1 <114995593+kalachandrasekar1@users.noreply.github.com>
2025-03-25 13:01:09 +0000

    Update judgmentPaidInFull.js

--- e2e/fixtures/events/claim/judgmentPaidInFull.js
+++ e2e/fixtures/events/claim/judgmentPaidInFull.js
@@ -4,7 +4,7 @@ module.exports = {
   valid: {
     MarkJudgmentPaidInFull: {
       joJudgmentPaidInFull: {
-        dateOfFullPaymentMade:  date,
+        dateOfFullPaymentMade:  date(0),
         confirmFullPaymentMade:['CONFIRMED']
       }
     }
