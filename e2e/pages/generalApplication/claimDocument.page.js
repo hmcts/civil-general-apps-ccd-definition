45e0c9ddf9ecbccf22f867e39d1d6f1a92de32a3
Paul Pearson <46532983+paul-pearson@users.noreply.github.com>
2025-04-24 13:00:22 +0100

    CIV-16847 Add event for updating language preference (#1455)
    * CIV-16847 Add event for updating language preference
    
    * CIV-16847 Expect 7 document sections
    
    * CIV-16847 Expect 6 document sections
    
    * CIV-16847 Draft app is penultimate element
    
    * CIV-16847 Draft app is penultimate element
    
    * CIV-16847 Temp comment out assertions
    
    * CIV-16847 Fix document position
    
    * CIV-16847 Fix document visibility assertions
    
    * CIV-16847 Reinstate and fix assertions
    
    * CIV-16847 Fix assertions
    
    * CIV-16847 Fix assertions
    
    * CIV-16847 Remove assertions with wrong number of sections
    
    * CIV-16847 Remove invalid assertion  - when without notice the draft app doc won't be seen
    
    * CIV-16847 Count could be 2 or 3 depending on with notice or not

--- e2e/pages/generalApplication/claimDocument.page.js
+++ e2e/pages/generalApplication/claimDocument.page.js
@@ -17,13 +17,14 @@ module.exports = {
   async verifyUploadedDocument(documentType) {
     await I.seeInCurrentUrl('documents');
     if (documentType === 'After SDO - Hearing Notice') {
-      await I.seeNumberOfVisibleElements(this.fields.docTitles, 5);
-    } else if (documentType === 'Free From Order' || documentType === 'Assisted Order') {
       await I.seeNumberOfVisibleElements(this.fields.docTitles, 6);
+    } else if (documentType === 'Free From Order' || documentType === 'Assisted Order') {
+      await I.seeNumberOfVisibleElements(this.fields.docTitles, 7);
     } else {
       await I.seeNumberOfVisibleElements(this.fields.docTitles, 5);
     }
-    let draftAppURL = await I.grabTextFrom(locate(this.fields.links).last());
+    let links = await I.grabTextFromAll(this.fields.links);
+    let draftAppURL = links[links.length - 2];
     expect(draftAppURL).to.contains(`Draft_application_${docFullDate}`);
 
     switch (documentType) {
@@ -58,11 +59,11 @@ module.exports = {
     let docType = await I.grabTextFrom(locate(this.fields.docLabel).last());
     if (documentType === 'After SDO - Hearing Notice') {
       await I.seeTextEquals('Hearing Notice', locate(this.fields.docLabel).at(5));
-      await I.seeTextEquals('Draft Application document', locate(this.fields.docLabel).last());
+      await I.seeTextEquals('Draft Application document', locate(this.fields.docLabel).at(6));
     } else if (documentType === 'Free From Order' || documentType === 'Assisted Order') {
       await I.seeTextEquals('General order document', locate(this.fields.docLabel).at(5));
       await I.seeTextEquals('Hearing Notice', locate(this.fields.docLabel).at(6));
-      await I.seeTextEquals('Draft Application document', locate(this.fields.docLabel).last());
+      await I.seeTextEquals('Draft Application document', locate(this.fields.docLabel).at(7));
     } else {
       expect(docType).to.equals(' Draft Application document');
     }
