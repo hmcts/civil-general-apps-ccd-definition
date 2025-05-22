30c5b07bffac247779a0bfe732c6a912685c7a7e
kdaHMCTS <kiyrean.dyer-allen@hmcts.net>
2025-05-22 11:36:25 +0100

    Run PR and Master functional tests on one codeceptjs runner

--- e2e/run-functional-tests.sh
+++ e2e/run-functional-tests.sh
@@ -3,15 +3,9 @@ set -ex
 
 export CCD_UI_TESTS=true
 if [ ${ENVIRONMENT} == preview ]; then
-  echo "Running FE tests on tests on ${ENVIRONMENT} env"
-  yarn test:master-e2e-tests
   echo "Running non prod tests on tests on ${ENVIRONMENT} env"
-  yarn test:apie2e-nonprod
-  echo "Running API tests on tests on ${ENVIRONMENT} env"
-  yarn test:api
+  yarn test:e2e-nonprod
 else
   echo "Running FE tests on tests on ${ENVIRONMENT} env"
   yarn test:master-e2e-tests
-  echo "Running API tests on tests on ${ENVIRONMENT} env"
-  yarn test:api
 fi
