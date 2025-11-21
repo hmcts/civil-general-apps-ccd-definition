1edf744e1b6c663a34100d7fd046a2cb0e2986c0
Shaun Francis <shaun.francis@hmcts.net>
2025-11-21 16:44:50 +0000

    Use single smoke test

--- e2e/tests/ui_tests/ga_smoke_test.js
+++ e2e/tests/ui_tests/ga_smoke_test.js
@@ -8,26 +8,26 @@ const claimAmountJudge = '11000';
 
 Feature('General Application Smoke tests @ga-smoke-tests');
 
-Scenario('GA 1v1  - Judge Makes Decision Order Made @smoke-tests', async ({api, I}) => {
-  civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', claimAmountJudge);
-  await api.amendClaimDocuments(config.applicantSolicitorUser);
-  await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
-  await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
-  await api.acknowledgeClaim(config.defendantSolicitorUser, civilCaseReference, true);
-  await api.defendantResponseClaim(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
-  await api.claimantResponseUnSpec(config.applicantSolicitorUser, mpScenario, 'JUDICIAL_REFERRAL');
-  console.log('Civil Case created for general application: ' + civilCaseReference);
-  console.log('Make a General Application');
-  gaCaseReference = await api.initiateGeneralApplicationWithOutNotice(config.defendantSolicitorUser, civilCaseReference);
-  console.log('Without Notice General Application Initiated by Defendant2 : ' + gaCaseReference);
-  console.log('*** Start Judge makes decision order made: ' + gaCaseReference + ' ***');
-  await api.judgeMakesDecisionOrderMade(config.judgeUser2WithRegionId2, gaCaseReference);
-  console.log('*** End Judge makes decision order made - GA Case Reference: ' + gaCaseReference + ' ***');
-
-  await I.login(config.defendantSolicitorUser);
-  await I.navigateToTab(civilCaseReference, 'Applications');
-  await I.see('Order Made');
-});
+// Scenario('GA 1v1  - Judge Makes Decision Order Made @smoke-tests', async ({api, I}) => {
+//   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser, mpScenario, 'Company', claimAmountJudge);
+//   await api.amendClaimDocuments(config.applicantSolicitorUser);
+//   await api.notifyClaim(config.applicantSolicitorUser, mpScenario, civilCaseReference);
+//   await api.notifyClaimDetails(config.applicantSolicitorUser, civilCaseReference);
+//   await api.acknowledgeClaim(config.defendantSolicitorUser, civilCaseReference, true);
+//   await api.defendantResponseClaim(config.defendantSolicitorUser, mpScenario, 'solicitorOne');
+//   await api.claimantResponseUnSpec(config.applicantSolicitorUser, mpScenario, 'JUDICIAL_REFERRAL');
+//   console.log('Civil Case created for general application: ' + civilCaseReference);
+//   console.log('Make a General Application');
+//   gaCaseReference = await api.initiateGeneralApplicationWithOutNotice(config.defendantSolicitorUser, civilCaseReference);
+//   console.log('Without Notice General Application Initiated by Defendant2 : ' + gaCaseReference);
+//   console.log('*** Start Judge makes decision order made: ' + gaCaseReference + ' ***');
+//   await api.judgeMakesDecisionOrderMade(config.judgeUser2WithRegionId2, gaCaseReference);
+//   console.log('*** End Judge makes decision order made - GA Case Reference: ' + gaCaseReference + ' ***');
+//
+//   await I.login(config.defendantSolicitorUser);
+//   await I.navigateToTab(civilCaseReference, 'Applications');
+//   await I.see('Order Made');
+// });
 
 Scenario('Claimant Hearing notice - Without notice journey @smoke-tests', async ({api, I}) => {
   civilCaseReference = await api.createUnspecifiedClaim(config.applicantSolicitorUser,
